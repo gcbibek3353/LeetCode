08a8edddb18ad5c923d4e84586f3deb6463acdec
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-01-09 22:49:06 +0530

    Time: 0 ms (100.00%) | Memory: 51.9 MB (54.84%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.ts
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.ts
@@ -2,9 +2,8 @@ function diagonalSum(mat: number[][]): number {
     const r = mat.length;
     let sum = 0;
     for(let i = 0; i < r; i ++){
-        for(let j = 0; j < r; j ++){
-            if(i === j || i+j === r - 1) sum += mat[i][j];
-        }
+       sum += mat[i][i];
+       if(i != r - 1 - i) sum += mat[i][r-1-i];
     }
     return sum;
 };
