29cce2827e0f63ba625ea7da9f83f7daa2d012af
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-01-21 21:18:09 +0530

    Time: 87 ms (8.95%) | Memory: 49.8 MB (94.87%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.ts
+++ 1013-fibonacci-number/fibonacci-number.ts
@@ -1,12 +1,5 @@
 function fib(n: number): number {
     if(n === 0) return 0;
     if(n === 1) return 1;
-    let n1 = 0;
-    let n2 = 1;
-    for(let i = 2; i <= n ; i ++){
-        let sum = n1 + n2;
-        n1 = n2;
-        n2 = sum;
-    }
-    return n2;
+    return fib(n-1) + fib(n-2);
 };
