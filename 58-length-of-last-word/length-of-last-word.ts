6917c233978b023b0331e1fda97f0da0efd0eb4b
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-10 21:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 51.6 MB (29.01%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.ts
+++ 58-length-of-last-word/length-of-last-word.ts
@@ -1,4 +1,12 @@
 function lengthOfLastWord(s: string): number {
-    let words = s.trim().split(' ');
-    return words[words.length - 1].length;
+    // let words = s.trim().split(' ');
+    // return words[words.length - 1].length;
+    let end = s.length - 1;
+    let length = 0;
+    while(end > 0 && s[end] === " ") end --;
+    while(end >= 0 && s[end] !== " "){
+        end --;
+        length ++;
+    }
+    return length;
 };
