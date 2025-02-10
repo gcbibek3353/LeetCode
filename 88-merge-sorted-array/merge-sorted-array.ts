42e6e30ea3ec15e597f3d3acdb3a8b8337f35207
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-10 20:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 51.4 MB (73.63%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.ts
+++ 88-merge-sorted-array/merge-sorted-array.ts
@@ -5,17 +5,14 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
     let i = m + n - 1;
     while ( i >= 0){
         if(nums1[p1] >= nums2[p2]){
-            console.log(nums1[p1]);
             nums1[i --] = nums1[p1 --];
             if(p1 < 0){
                 while(p2 >= 0){
-                    console.log(nums2[p2]);
                      nums1[i --] = nums2[p2 --]};
             break;
             }
         }
         else{
-            console.log(nums2[p2])
             nums1[i --] = nums2[p2 --];
             if(p2 < 0) break;
         }
