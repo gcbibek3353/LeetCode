aa10f4ca885c0f47534c6118f1d25c59dacb4107
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-13 13:17:57 +0530

    Time: 20 ms (45.40%) | Memory: 53.3 MB (72.19%) - LeetSync

---
+++ 383-ransom-note/ransom-note.ts
@@ -0,0 +1,28 @@
+function canConstruct(ransomNote: string, magazine: string): boolean {
+    let canConstruct = true;
+    const ransomNoteMap = new Map();
+    const magazineMap = new Map();
+    for(let i = 0; i < ransomNote.length; i ++){
+        if(!ransomNoteMap.get(ransomNote.charAt(i))){
+            ransomNoteMap.set(ransomNote.charAt(i),1);
+        }
+        else
+        ransomNoteMap.set(ransomNote.charAt(i),ransomNoteMap.get(ransomNote.charAt(i)) + 1);
+    }
+    for(let i = 0; i < magazine.length; i ++){
+        if(!magazineMap.get(magazine.charAt(i))){
+            magazineMap.set(magazine.charAt(i),1);
+        }
+        else
+        magazineMap.set(magazine.charAt(i),magazineMap.get(magazine.charAt(i)) + 1);
+    }
+    // console.log(ransomNoteMap);
+    // console.log(magazineMap);
+    ransomNoteMap.forEach((value,key)=>{
+        // console.log(value);
+        // console.log(key);
+        // console.log(magazineMap.get(key));
+        if((magazineMap.get(key) == undefined ) || (magazineMap.get(key) < value)) canConstruct = false;
+    })
+    return canConstruct;
+};
