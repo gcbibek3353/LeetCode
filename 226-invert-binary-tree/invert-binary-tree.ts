8594cf079cf8733de979e8d250a2c29dccfe1eac
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-24 11:24:44 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (8.18%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.ts
+++ 226-invert-binary-tree/invert-binary-tree.ts
@@ -14,11 +14,11 @@
 
 function invertTree(root: TreeNode | null): TreeNode | null {
     if(root === null) return null;
+    invertTree(root.left);
+    invertTree(root.right);
     let temp = new TreeNode();
     temp = root.left;
     root.left = root.right;
     root.right = temp;
-    invertTree(root.left);
-    invertTree(root.right);
     return root;
 };
