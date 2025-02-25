a36e41aa8adf989202ea2dea2df81aea9956850f
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-25 23:10:45 +0530

    Time: 4 ms (96.83%) | Memory: 59.7 MB (6.64%) - LeetSync

--- 383-ransom-note/ransom-note.ts
+++ 383-ransom-note/ransom-note.ts
@@ -1,7 +1,14 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-    for(const char of magazine){
-        ransomNote = ransomNote.replace(char, "");
-    }
-    if(!ransomNote) return true;
-    else return false;
+    if (ransomNote.length > magazine.length) return false;
+
+    const array: string[] = magazine.split("");
+
+    for (const char of ransomNote) {
+        const index: number = array.indexOf(char);
+        if (index === -1) return false;
+
+        array[index] = "";
+    };
+
+    return true;
 };
