c8455422a91c5569356e39eba9a96316af3b6eec
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-25 23:07:24 +0530

    Time: 13 ms (76.82%) | Memory: 60.9 MB (5.19%) - LeetSync

--- 383-ransom-note/ransom-note.ts
+++ 383-ransom-note/ransom-note.ts
@@ -1,23 +1,7 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
-    let canConstruct = true;
-    const ransomNoteMap = new Map();
-    const magazineMap = new Map();
-    for(let i = 0; i < ransomNote.length; i ++){
-        if(!ransomNoteMap.get(ransomNote.charAt(i))){
-            ransomNoteMap.set(ransomNote.charAt(i),1);
-        }
-        else
-        ransomNoteMap.set(ransomNote.charAt(i),ransomNoteMap.get(ransomNote.charAt(i)) + 1);
+    for(const char of magazine){
+        ransomNote = ransomNote.replace(char, "");
     }
-    for(let i = 0; i < magazine.length; i ++){
-        if(!magazineMap.get(magazine.charAt(i))){
-            magazineMap.set(magazine.charAt(i),1);
-        }
-        else
-        magazineMap.set(magazine.charAt(i),magazineMap.get(magazine.charAt(i)) + 1);
-    }
-    ransomNoteMap.forEach((value,key)=>{
-        if((magazineMap.get(key) == undefined ) || (magazineMap.get(key) < value)) canConstruct = false;
-    })
-    return canConstruct;
+    if(!ransomNote) return true;
+    else return false;
 };
