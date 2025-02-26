70fefb6f5aaad466d716202bd6f4d6dc1ed1a02a
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-26 22:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (8.37%) - LeetSync

--- 228-summary-ranges/summary-ranges.ts
+++ 228-summary-ranges/summary-ranges.ts
@@ -1,15 +1,10 @@
 function summaryRanges(nums: number[]): string[] {
     const ans = [];
-    let i = 0;
-    while(i < nums.length){
-        const curStartIndex = i;
-        while(i < nums.length && ((nums[i] + 1) === nums[i+1])){
-            i ++;
-        }
-        const curEndIndex = i;
-        if(curStartIndex === curEndIndex) ans.push(String(nums[curStartIndex]))
-        else ans.push(`${nums[curStartIndex]}->${nums[curEndIndex]}`);
-        i ++;
+    for(let i = 0; i < nums.length; i ++){
+        const startIndex = i;
+        while(nums[i] + 1 === nums[i + 1]) i ++;
+        if(startIndex === i) ans.push(String(nums[i]));
+        else ans.push(`${nums[startIndex]}->${nums[i]}`);
     }
     return ans;
 };
