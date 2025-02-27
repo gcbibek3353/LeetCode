5637b804144e8b298941c129047b061698cbaf41
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-27 21:26:22 +0530

    Time: 45 ms (96.88%) | Memory: 58.5 MB (6.72%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.ts
@@ -0,0 +1,10 @@
+function reverseBits(n: number): number {
+    let ans = 0
+
+    for (let i = 0; i < 32; i++) {
+        ans |= (n & 1) << (31 - i)
+        n >>= 1
+    }
+
+    return ans >>> 0
+};
