f6a7ba44d527ce26b5defaa641c01c3fd0ccb3ac
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-02-27 08:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (7.17%) - LeetSync

--- 66-plus-one/plus-one.ts
+++ 66-plus-one/plus-one.ts
@@ -1,6 +1,15 @@
 function plusOne(digits: number[]): number[] {
-    let digit = digits.join('')
 
-    let numDigit = BigInt(digit)+ BigInt(1)
-    return numDigit.toString().split('').map(Number)
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] += 1;
+            return digits;
+        }
+        digits[i] = 0;
+    }
+    
+    const newDigits = new Array(digits.length + 1).fill(0);
+    newDigits[0] = 1;
+
+    return newDigits;
 };
