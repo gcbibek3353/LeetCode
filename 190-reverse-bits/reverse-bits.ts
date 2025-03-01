d39f7109e1740552908365fabc88b71d00780590
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-03-01 10:06:21 +0530

    Time: 42 ms (98.55%) | Memory: 57.7 MB (13.25%) - LeetSync

--- 190-reverse-bits/reverse-bits.ts
+++ 190-reverse-bits/reverse-bits.ts
@@ -1,10 +1,10 @@
 function reverseBits(n: number): number {
     let ans = 0
-
-    for (let i = 0; i < 32; i++) {
-        ans |= (n & 1) << (31 - i)
-        n >>= 1
+    for(let i = 0; i < 32; i ++){
+        let LSB = n & 1;
+        let correctPos = 31 - i;
+        ans = ans | LSB << correctPos;
+        n >>= 1;
     }
-
-    return ans >>> 0
+    return ans >>> 0;
 };
