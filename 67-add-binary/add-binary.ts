28d138047b3d406ec23cd8652d1367b20c158bd4
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-03-01 10:20:39 +0530

    Time: 2 ms (59.50%) | Memory: 56.8 MB (20.12%) - LeetSync

--- 67-add-binary/add-binary.ts
+++ 67-add-binary/add-binary.ts
@@ -1,21 +1,16 @@
 function addBinary(a: string, b: string): string {
-    let result = "";
-    let lenA = a.length -1;
-    let lenB = b.length -1;
-    let carry = 0;
-
-    while(lenA >= 0 || lenB >= 0 || carry > 0) {
-        let bitA = lenA >= 0 ? parseInt(a[lenA]) : 0;
-        let bitB = lenB >= 0 ? parseInt(b[lenB]) : 0;
-
-        const sum = bitA + bitB + carry;
-
-        result = (sum % 2).toString() + result;
-        carry = Math.floor(sum / 2);
-
-        lenA--;
-        lenB--;
-    }
-
-    return result;
+   let ans = "";
+   let aP = a.length - 1;
+   let bP = b.length - 1;
+   let carry = 0;
+   while(aP >= 0 || bP >= 0 || carry > 0){
+    const aDigit = (aP >= 0) ? parseInt(a[aP]) : 0;
+    const bDigit = (bP >= 0 ? parseInt(b[bP]) : 0);
+    const sum = aDigit + bDigit + carry;
+    ans = (sum % 2) + ans;
+    carry = Math.floor(sum / 2);
+    aP--;
+    bP --;
+   }
+   return ans;
 };
