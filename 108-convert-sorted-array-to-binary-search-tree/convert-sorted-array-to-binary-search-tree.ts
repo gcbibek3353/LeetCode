2c6f6cbebcb033c4e949c32be9fe25541fff52b1
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-03-01 10:38:06 +0530

    Time: 3 ms (29.98%) | Memory: 60 MB (7.31%) - LeetSync

--- 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.ts
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.ts
@@ -13,11 +13,14 @@
  */
 
 function sortedArrayToBST(nums: number[]): TreeNode | null {
-    if (nums.length === 0) return null
-
-    const centerIndex = Math.floor(nums.length / 2)
-    return new TreeNode(nums[centerIndex],
-        sortedArrayToBST(nums.slice(0, centerIndex)),
-        sortedArrayToBST(nums.slice(centerIndex + 1))
+  if(nums.length === 0) return null;
+   const midIndex = Math.floor(nums.length / 2);
+   const curValue = nums[midIndex];
+   
+   return new TreeNode(
+    curValue,
+    sortedArrayToBST(nums.slice(0,midIndex)),
+    sortedArrayToBST(nums.slice(midIndex + 1))
     )
-};
+}
+      
