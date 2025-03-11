76766446d1dfdbd78f5f4f66b3dbfb333a0d7dc5
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-03-11 07:35:00 +0530

    Time: 1 ms (84.14%) | Memory: 58.8 MB (13.34%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.ts
@@ -0,0 +1,16 @@
+function jump(nums: number[]): number {
+    let near = 0;
+    let far = 0;
+    let jumps = 0;
+
+    while(far < nums.length - 1){
+        let farthest = 0;
+        for(let i = near; i <= far; i ++){
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+        near = far + 1;
+        far = farthest;
+        jumps ++;
+    }
+    return jumps;
+};
