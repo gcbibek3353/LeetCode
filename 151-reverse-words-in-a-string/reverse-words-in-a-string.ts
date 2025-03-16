58222d3c7ba56c7abd3fce1e96344f93eb93b891
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-03-16 07:21:21 +0530

    Time: 1 ms (81.31%) | Memory: 57.9 MB (16.29%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -0,0 +1,13 @@
+function reverseWords(s: string): string {
+  
+ let words = s.split(' ');
+    let res = [];
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        if (words[i]) {
+            res.push(words[i]);
+        }
+    }
+
+    return res.join(' ');
+};
