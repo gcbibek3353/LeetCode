20de4256bf9842af960f8a639614d3a0559b47ca
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-03-16 08:56:24 +0530

    Time: 3 ms (29.02%) | Memory: 56.7 MB (12.60%) - LeetSync

--- 392-is-subsequence/is-subsequence.ts
+++ 392-is-subsequence/is-subsequence.ts
@@ -1,12 +1,8 @@
 function isSubsequence(s: string, t: string): boolean {
-    if(s.length <= 0) return true;
-
-    let j = 0;
-    for(let i = 0; i < t.length; i ++){
-        if(t.charAt(i) === s.charAt(j)){
-            j ++;
-            if(j === s.length) return true;
-        }
-    }
-    return false;
+   if(s.length > t.length) return false;
+   let sP = 0;
+   for(let i = 0; i < t.length; i ++){
+    if(t.charAt(i) === s[sP]) sP ++;
+   }
+   return (sP === s.length)
 };
