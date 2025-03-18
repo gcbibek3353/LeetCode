f173463b0a5cfbb84145400bb30eb7a38da5dea2
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-03-18 09:55:44 +0530

    Time: 1 ms (86.31%) | Memory: 60.1 MB (42.53%) - LeetSync

--- 55-jump-game/jump-game.ts
+++ 55-jump-game/jump-game.ts
@@ -1,7 +1,7 @@
 function canJump(nums: number[]): boolean {
-  let target = nums.length - 1;
-  for(let i = nums.length - 2; i >= 0; i --){
-    if(i + nums[i] >= target) target = i;
-  }
-  return target === 0;
+    let target = nums.length - 1;
+    for(let i = nums.length - 2; i >= 0; i --){
+        if(nums[i] + i >= target) target = i;
+    }
+    return target === 0;
 };
