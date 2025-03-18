84dcffce2fc978c86c881c2ac3121ce51594f1f0
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-03-18 13:01:31 +0530

    Time: 6 ms (69.96%) | Memory: 70.1 MB (35.86%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.ts
+++ 238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,16 +1,15 @@
 function productExceptSelf(nums: number[]): number[] {
-    let L = nums[0];
-    let ans = [];
-    for(let i = nums.length - 1; i >= 0; i -- ){
-        ans[i] = (i === nums.length - 1) ? nums[i] : nums[i] * ans[i + 1];
-    }
-    for(let i = 0; i < nums.length; i ++){
-        if(i === 0) ans[i] = ans[i + 1];
-        else if(i === nums.length - 1) ans[i] = L;
-        else{
-            ans[i] = L * ans[i + 1];
-            L *= nums[i];
-        }
-    }
-    return ans;
+    const l = nums.length;
+  const res = new Array(l).fill(1);
+  let prefix = 1,
+    suffix = 1;
+
+  for (let i = 0; i < l; i++) {
+    res[i] *= prefix;
+    res[l - 1 - i] *= suffix;
+    prefix *= nums[i];
+    suffix *= nums[l - 1 - i];
+  }
+
+  return res;
 };
