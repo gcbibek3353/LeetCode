4abd4aa0d6770116d901e23eb7d7fa4b611d177d
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-04-02 07:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 58 MB (45.65%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.ts
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.ts
@@ -1,8 +1,9 @@
 function maximumTripletValue(nums: number[]): number {
+    let n = nums.length;
     let maxValue = -Infinity;
-    for(let i = 0; i <= nums.length - 3; i ++){
-        for(let j = i + 1; j <= nums.length - 2; j ++){
-            for(let k = j + 1; k <= nums.length - 1; k ++) {
+    for(let i = 0; i <= n - 3; i ++){
+        for(let j = i + 1; j <= n - 2; j ++){
+            for(let k = j + 1; k <= n - 1; k ++) {
                 let value = (nums[i] - nums[j]) * nums[k];
                 maxValue = Math.max(maxValue, value);
             }
