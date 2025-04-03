bdb5dc8a3d64f895d2883e94b8929199e9ac35e7
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-04-03 07:41:21 +0530

    Time: 2 ms (95.45%) | Memory: 68.4 MB (63.64%) - LeetSync

--- 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.ts
+++ 3152-maximum-value-of-an-ordered-triplet-ii/maximum-value-of-an-ordered-triplet-ii.ts
@@ -1,18 +1,13 @@
 function maximumTripletValue(nums: number[]): number {
-    let n = nums.length;
-    let leftMax = new Array(n).fill(0)
-    let rightMax = new Array(n).fill(0);
-    
-    for(let i = 0; i < n; i ++){
-        leftMax[i] = (i === 0) ? nums[i] : Math.max(leftMax[i - 1],nums[i]);
-        rightMax[n - i - 1] = (i === 0) ? nums[n - i - 1] : Math.max(rightMax[n - i],nums[n - i - 1]);
-    }
-    let res = 0;
-    // console.log(leftMax);
-    // console.log(rightMax);
-    for(let j = 1; j <= n - 2; j ++){
-        res = Math.max(res, (leftMax[j - 1] - nums[j]) * rightMax[j + 1]);
-        // console.log(res,j);
+  const n = nums.length;
+  let res = 0;
+  let imax = 0;
+  let maxDiff = 0;
+
+    for(let k = 0; k < n; k ++){
+        res = Math.max(res,maxDiff * nums[k]);
+        imax = Math.max(imax,nums[k]);
+        maxDiff = Math.max(maxDiff,imax - nums[k]);
     }
     return res;
 };
