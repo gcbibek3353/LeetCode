bf17c4dab9444c2933acfe3fdcef5e4b6e504a58
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-04-14 09:56:00 +0530

    Time: 50 ms (89.29%) | Memory: 56.2 MB (42.86%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.ts
+++ 1656-count-good-triplets/count-good-triplets.ts
@@ -2,15 +2,12 @@ function countGoodTriplets(arr: number[], a: number, b: number, c: number): numb
     let count = 0;
     const n = arr.length;
 
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            if (Math.abs(arr[i] - arr[j]) <= a) {
-                for (let k = j + 1; k < n; k++) {
-                    if (
-                        Math.abs(arr[j] - arr[k]) <= b &&
-                        Math.abs(arr[i] - arr[k]) <= c
-                    ) {
-                        count++;
+    for(let i = 0; i < n; i ++){
+        for(let j = i + 1; j < n; j ++){
+            if(Math.abs(arr[i] - arr[j]) <= a){
+                for(let k = j + 1; k < n; k ++){
+                    if(Math.abs(arr[i] - arr[k]) <= c && Math.abs(arr[j] - arr[k]) <= b){
+                        count ++;
                     }
                 }
             }
