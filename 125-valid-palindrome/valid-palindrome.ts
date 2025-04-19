301f72e1b06b0dc071b9fa132f7a4da5fe913c5c
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-04-19 08:16:12 +0530

    Time: 7 ms (34.48%) | Memory: 61.9 MB (26.91%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -2,41 +2,15 @@ function isAlphaNumeric(char: string): boolean {
   return /[a-zA-Z0-9]/.test(char); // Check if the character is alphanumeric
 }
 
-// function checkPalindrome(s: string, l: number, r: number): boolean {
-//   while (l < r) {
-//     // Skip non-alphanumeric characters
-//     while (l < r && !isAlphaNumeric(s.charAt(l))) l++;
-//     while (l < r && !isAlphaNumeric(s.charAt(r))) r--;
-
-//     // Check for mismatch
-//     if (s.charAt(l).toLowerCase() !== s.charAt(r).toLowerCase()) {
-//       return false;
-//     }
-
-//     l++;
-//     r--;
-//   }
-//   return true;
-// }
-
 function isPalindrome(s: string): boolean {
-  if (s.length <= 1) return true; // Single characters or empty strings are palindromes
-//   return checkPalindrome(s, 0, s.length - 1);
-    let l = 0;
-    let r = s.length - 1;
-     while (l < r) {
-//     // Skip non-alphanumeric characters
-    while (l < r && !isAlphaNumeric(s.charAt(l))) l++;
-    while (l < r && !isAlphaNumeric(s.charAt(r))) r--;
-
-    // Check for mismatch
-    if (s.charAt(l).toLowerCase() !== s.charAt(r).toLowerCase()) {
-      return false;
-    }
-
-    l++;
-    r--;
-  }
-  return true;
-
-}
+   let l = 0;
+   let r = s.length - 1;
+   while(l < r){
+    while(l < r && !isAlphaNumeric(s.charAt(l))) l ++;
+    while(l < r && !isAlphaNumeric(s.charAt(r))) r --;
+    if(s.charAt(r).toLowerCase() !== s.charAt(l).toLowerCase()) return false;
+    l ++;
+    r --;
+   }
+   return true;
+};
