1aa1a6d2e9fefa13b50cb831e749429ef682f748
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-06-03 20:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (62.91%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.ts
+++ 88-merge-sorted-array/merge-sorted-array.ts
@@ -1,18 +1,10 @@
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let p1 = m - 1;
-    let p2 = n - 1;
-    //    for(let i = nums1.length - 1 ; i >= 0; i --){
-    //         if(nums2[p2] >= nums1[p1] && p2 >= 0) nums1[i] = nums2[p2 --];
-    //         else if(nums2[p2] < nums1[p1] && p1 >= 0) nums1[i] = nums1[p1 --];
-    //    }
-    let i = nums1.length - 1;
-    while (p1 >= 0 && p2 >= 0) {
-        if (p2 < 0) return;
-        if (p1 < 0) break;
-        if (nums2[p2] >= nums1[p1]) nums1[i --] = nums2[p2--];
-        else nums1[i --] = nums1[p1--];
-    }
-    while(p2 >= 0){
-        nums1[p2] = nums2[p2 --];
-    }
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let i = nums1.length - 1;
+  while(p1 >= 0 && p2 >= 0) {
+    if(nums2[p2] >= nums1[p1]) nums1[i --] = nums2[p2 --];
+    else nums1[i --] = nums1[p1 --];
+  }
+  if(p1 < 0 && p2 >= 0) while(p2 >= 0) nums1[i --] = nums2[p2 --];
 };
