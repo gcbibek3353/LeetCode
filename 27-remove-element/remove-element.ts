6e5645562fbceebf28b31bc58993375e5272f190
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-06-28 08:21:38 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (69.51%) - LeetSync

--- 27-remove-element/remove-element.ts
+++ 27-remove-element/remove-element.ts
@@ -1,6 +1,7 @@
 function removeElement(nums: number[], val: number): number {
-    let p = 0;
-    for(let i = 0; i < nums.length; i ++)
-        if(nums[i] != val) nums[p ++] = nums[i];
-    return p;
+    let j = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) nums[j++] = nums[i];
+    }
+    return j;
 };
