8907c9117c2e6614dee71f378fbcff1cbe7b6658
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-06-28 08:37:49 +0530

    Time: 0 ms (100.00%) | Memory: 56.2 MB (86.23%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -1,10 +1,13 @@
 function twoSum(numbers: number[], target: number): number[] {
-    let p1 = 0;
-    let p2 = numbers.length - 1;
-    while(p1 < p2){
-        if(numbers[p1] + numbers[p2] === target) return [p1 + 1, p2 + 1];
-        else if(numbers[p1] + numbers[p2] > target) p2 --;
-        else p1 ++;
+    let l = 0;
+    let r = numbers.length - 1;
+    while(l < r){
+        const curSum = numbers[l] + numbers[r];
+        if(curSum < target) l ++;
+        else if (curSum > target) r --;
+        else {
+            let ans = [l + 1, r + 1];
+            return ans;
+        }
     }
-    
 };
