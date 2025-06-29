1c3228845e4d87a26a7c6ae12435fe58117341ab
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-06-29 08:54:41 +0530

    Time: 3 ms (39.26%) | Memory: 64 MB (25.67%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.ts
+++ 11-container-with-most-water/container-with-most-water.ts
@@ -2,16 +2,16 @@ function maxArea(height: number[]): number {
     let l = 0;
     let r = height.length - 1;
     let maxArea = 0;
-    while(l < r){
-        if(height[l] < height[r]){
-            let curArea = height[l] * (r - l);
-            maxArea = Math.max(maxArea,curArea);
-            l ++;
+    while (l < r) {
+        if (height[l] < height[r]) {
+            const area = height[l] * (r - l);
+            maxArea = Math.max(area, maxArea);
+            l++;
         }
-        else{
-            let curArea = height[r] * (r - l);
-            maxArea = Math.max(maxArea,curArea);
-            r --;
+        else {
+            const area = height[r] * (r - l);
+            maxArea = Math.max(area, maxArea);
+            r--;
         }
     }
     return maxArea;
