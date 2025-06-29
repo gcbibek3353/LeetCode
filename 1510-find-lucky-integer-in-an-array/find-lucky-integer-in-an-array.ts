edc52dc42c2fb976a77d0923549e8f7b1c24b9d6
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-06-29 09:43:46 +0530

    Time: 3 ms (60.42%) | Memory: 58 MB (62.50%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.ts
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.ts
@@ -1,18 +1,7 @@
 function findLucky(arr: number[]): number {
-    const mp = new Map<number, number>();
+  const numbers: Map<number, number> = new Map();
 
-    for (let i = 0; i < arr.length; i++) {
-        if (mp.has(arr[i])) {
-            mp.set(arr[i], mp.get(arr[i]) + 1);
-        }
-        else {
-            mp.set(arr[i], 1);
-        }
-    }
+  arr.forEach(num => numbers.set(num, (numbers.get(num) || 0) + 1));
 
-    let ans = -1;
-    for (let [key, value] of mp.entries()) {
-        if (key === value) ans = Math.max(ans, key);
-    }
-    return ans;
+  return Array.from(numbers.keys()).sort((a, b) => b - a).find(num => num === numbers.get(num)) || -1;
 };
