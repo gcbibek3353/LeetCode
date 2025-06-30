e41476561f16e634520f0410cc4b6e27763d7cba
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-06-30 08:29:48 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (78.94%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -1,20 +1,13 @@
 function search(nums: number[], target: number): number {
-    let startIndex = 0;
-    let endIndex = nums.length - 1;
-
-    while (startIndex <= endIndex) {
-        const middleIndex = Math.floor((startIndex + endIndex) / 2);
-        const middleValue = nums[middleIndex];
-
-        if (middleValue === target) {
-            return middleIndex;
-        }
-        else if (middleValue < target) {
-            startIndex = middleIndex + 1;
-        } else {
-            endIndex = middleIndex - 1;
-        }
+    if (nums.length === 0) return -1;
+    if (nums.length === 1) return (nums[0] === target) ? 0 : -1;
+    let l = 0;
+    let r = nums.length - 1;
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        if (nums[mid] === target) return mid;
+        else if (nums[mid] < target) l = mid + 1;
+        else r = mid - 1;
     }
-
     return -1;
-}
+};
