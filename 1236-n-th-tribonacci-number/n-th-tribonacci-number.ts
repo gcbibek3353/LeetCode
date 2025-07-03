2fe801a349483a663b0db14f5e6753c1565bab28
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-03 08:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 53.8 MB (88.12%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.ts
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.ts
@@ -1,11 +1,6 @@
+    let arr = [0, 1, 1];
 function tribonacci(n: number): number {
-    let memo = [0];
-    function tribo(n: number): number {
-        if(memo[n]) return memo[n];
-        if (n === 0) return 0;
-        if (n === 1) return 1;
-        if (n === 2) return 1;
-        return memo[n] = tribo(n - 1) + tribo(n - 2) + tribo(n - 3);
-    }
-    return tribo(n);
+    if (n === 0) return 0;
+    if (arr[n]) return arr[n];
+    return arr[n] = (tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3));
 };
