190660668312980d5ae1f2ddd0738a238e928310
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-03 08:06:16 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (51.72%) - LeetSync

--- 231-power-of-two/power-of-two.ts
+++ 231-power-of-two/power-of-two.ts
@@ -1,9 +1,4 @@
 function isPowerOfTwo(n: number): boolean {
     if(n <= 0) return false;
-    if(n === 1) return true;
-    while(n > 1){
-        n = n / 2;
-        if(n === 1) return true;
-    }
-    return false;
+    return (n & (n - 1)) === 0;
 };
