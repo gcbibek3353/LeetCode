40087d6928b4b983f06fb5e7d4b90e1e472ec659
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-09 08:40:43 +0530

    Time: 8 ms (34.94%) | Memory: 66.3 MB (55.10%) - LeetSync

--- 189-rotate-array/rotate-array.ts
+++ 189-rotate-array/rotate-array.ts
@@ -2,11 +2,10 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-  let result = new Array<number>();
-  for(let i = 0; i < nums.length; i ++){
-    result[(i + k ) % nums.length] = nums[i];
-  }
-  for(let i = 0; i < nums.length; i ++) {
-    nums[i] = result[i];
-  }
+    let ans = [];
+    let n = nums.length;
+    for (let i = 0; i < n; i++) {
+        ans[(i + k) % n] = nums[i];
+    }
+    for (let i = 0; i < n; i++) nums[i] = ans[i];
 };
