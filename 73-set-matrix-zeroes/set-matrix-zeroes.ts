4b71fabaf3c92a3e8b82764ed472da42ec580c39
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-11 08:17:41 +0530

    Time: 6 ms (27.45%) | Memory: 59.9 MB (57.68%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.ts
+++ 73-set-matrix-zeroes/set-matrix-zeroes.ts
@@ -4,31 +4,49 @@
 function setZeroes(matrix: number[][]): void {
     let r = matrix.length;
     let c = matrix[0].length;
-    let rows = new Array(r);
-    let cols = new Array(c);
+    let first_row_contains_zero = false;
+    let first_col_contains_zero = false;
+
     for (let i = 0; i < r; i++) {
-        for (let j = 0; j < c; j++) {
+        if (matrix[i][0] === 0) first_col_contains_zero = true;
+    }
+
+    for (let i = 0; i < c; i++) {
+        if (matrix[0][i] === 0) first_row_contains_zero = true;
+    }
+
+    for (let i = 1; i < r; i++) {
+        for (let j = 1; j < c; j++) {
             if (matrix[i][j] === 0) {
-                rows[i] = 0;
-                cols[j] = 0;
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
             }
         }
     }
 
-    for (let i = 0; i < rows.length; i++) {
-        if (rows[i] === 0) {
+    for (let i = 1; i < r; i++) {
+        if (matrix[i][0] === 0) {
             for (let j = 0; j < c; j++) {
                 matrix[i][j] = 0;
             }
         }
     }
 
-    for (let i = 0; i < cols.length; i++) {
-        if (cols[i] === 0) {
+    for (let i = 1; i < c; i++) {
+        if (matrix[0][i] === 0) {
             for (let j = 0; j < r; j++) {
                 matrix[j][i] = 0;
             }
         }
     }
+
+    if (first_row_contains_zero) {
+        for (let i = 0; i < c; i++) matrix[0][i] = 0;
+    }
+
+    if (first_col_contains_zero) {
+        for (let i = 0; i < r; i++) matrix[i][0] = 0;
+    }
+
 
 };
