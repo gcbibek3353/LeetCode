7faf01d4c0a48f4e996a68101abeab4cfc86e5c5
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-11 08:02:39 +0530

    Time: 2 ms (80.97%) | Memory: 59.4 MB (85.83%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.ts
+++ 73-set-matrix-zeroes/set-matrix-zeroes.ts
@@ -2,49 +2,33 @@
  Do not return anything, modify matrix in-place instead.
  */
 function setZeroes(matrix: number[][]): void {
-    let firstRowContainsZero = false;
-    let firstColContainsZero = false;
-    // Checking if first row contains zero
-    for(let i = 0; i < matrix[0].length; i ++){
-        if(matrix[0][i] === 0) firstRowContainsZero = true;
-    }
-    // checking if first col contains zero
-    for(let i = 0; i < matrix.length; i ++){
-        if(matrix[i][0] === 0) firstColContainsZero = true;
-    }
-    // making first row and first col zero whose row or cols contains zero
-    for(let r = 1; r < matrix.length ; r ++){
-        for(let c = 1; c < matrix[0].length; c ++){
-            if(matrix[r][c] === 0){
-                matrix[r][0] = 0;
-                matrix[0][c] = 0;
+    let r = matrix.length;
+    let c = matrix[0].length;
+    let rows = new Array(r);
+    let cols = new Array(c);
+    for (let i = 0; i < r; i++) {
+        for (let j = 0; j < c; j++) {
+            if (matrix[i][j] === 0) {
+                rows[i] = 0;
+                cols[j] = 0;
             }
         }
     }
 
-    console.log(matrix);
-    // setting rows with zeros 
-    for(let i = 1 ; i < matrix.length; i ++) {
-        if(matrix[i][0] === 0){
-            for(let j = 0; j < matrix[0].length; j ++){
+    for (let i = 0; i < rows.length; i++) {
+        if (rows[i] === 0) {
+            for (let j = 0; j < c; j++) {
                 matrix[i][j] = 0;
             }
         }
     }
-    //setting cols with zeros
-    for(let i = 1; i < matrix[0].length; i ++){
-        if(matrix[0][i] === 0){
-            for(let j = 0; j < matrix.length; j ++){
+
+    for (let i = 0; i < cols.length; i++) {
+        if (cols[i] === 0) {
+            for (let j = 0; j < r; j++) {
                 matrix[j][i] = 0;
             }
         }
     }
-    // if first row contains zero make all it's elements zero
-    if(firstRowContainsZero){
-        for(let i = 0; i < matrix[0].length; i ++) matrix[0][i] = 0;
-    }
-    // if first col contains zero make all it's elements zero
-    if(firstColContainsZero){
-        for(let i = 0; i < matrix.length; i ++) matrix[i][0] = 0;
-    }
+
 };
