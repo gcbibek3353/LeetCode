f9aeb27ff7e23b25b6c9316ded8f2d192534dbe9
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-11 08:30:13 +0530

    Time: 0 ms (100.00%) | Memory: 56.8 MB (25.85%) - LeetSync

--- 48-rotate-image/rotate-image.ts
+++ 48-rotate-image/rotate-image.ts
@@ -2,17 +2,12 @@
  Do not return anything, modify matrix in-place instead.
  */
 function rotate(matrix: number[][]): void {
-//   Two ways : (transpose and horizontal rotate) or (vertical rotate and transpose)
-// transpose and horizontal rotate (horizontal rotate takes O(n*log(n))
-// vertical rotate and transpose (vertical rotate takes O(log(n))) only
-
-// vertical rotate
-let l = 0; 
-let r = matrix.length - 1;
-while(l < r) [matrix[l ++], matrix[r --]] = [matrix[r],matrix[l]];
-for(let i = 0; i < matrix.length; i ++){
-    for(let j = i + 1; j < matrix.length; j ++){
-        [matrix[i][j],matrix[j][i]] = [matrix[j][i],matrix[i][j]];
+    let l = 0;
+    let r = matrix.length - 1;
+    while (l < r) [matrix[l++], matrix[r--]] = [matrix[r], matrix[l]];
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = i + 1; j < matrix.length; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
     }
-}
 };
