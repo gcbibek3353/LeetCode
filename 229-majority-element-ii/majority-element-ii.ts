6a17993a340b7a14d5fc137d40c5a1907d64cd1b
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-12 08:30:25 +0530

    Time: 1 ms (97.91%) | Memory: 57.9 MB (95.81%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.ts
+++ 229-majority-element-ii/majority-element-ii.ts
@@ -1,16 +1,35 @@
 function majorityElement(nums: number[]): number[] {
-    let ans = [];
-    const n = Math.floor(nums.length / 3);
-    let mp = new Map<number, number>();
+    let cnt1 = 0, cnt2 = 0;
+    let el1 = -Infinity, el2 = -Infinity;
 
     for (let i = 0; i < nums.length; i++) {
-        mp.set(nums[i], (mp.get(nums[i]) || 0) + 1);
+        if (cnt1 === 0 && nums[i] !== el2) {
+            cnt1++;
+            el1 = nums[i];
+        }
+        else if (cnt2 === 0 && nums[i] !== el1) {
+            cnt2++;
+            el2 = nums[i];
+        }
+        else if (nums[i] === el1) cnt1++;
+        else if (nums[i] === el2) cnt2++;
+        else {
+            cnt1--;
+            cnt2--;
+        }
     }
 
-    for (let [key, value] of mp.entries()) {
-        if (value > n) ans.push(key);
+    cnt1 = 0;
+    cnt2 = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === el1) cnt1++;
+        if (nums[i] === el2) cnt2++;
     }
+
+    let ans = [];
+    let mini = Math.floor(nums.length / 3);
+    if (cnt1 > mini) ans.push(el1);
+    if (cnt2 > mini) ans.push(el2);
 
     return ans;
-
 };
