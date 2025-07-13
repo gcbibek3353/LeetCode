7270dc8029bdab12af0159d8eca23b938b649c86
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-13 08:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (30.23%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -1,28 +1,28 @@
 function search(nums: number[], target: number): number {
-  let left = 0, right = nums.length - 1;
+    let left = 0, right = nums.length - 1;
 
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
 
-    if (nums[mid] === target) return mid;
+        if (nums[mid] === target) return mid;
 
-    // Left half is sorted
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) {
-        right = mid - 1; // search left
-      } else {
-        left = mid + 1; // search right
-      }
+        // Left half is sorted
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1; // search left
+            } else {
+                left = mid + 1; // search right
+            }
+        }
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1; // search right
+            } else {
+                right = mid - 1; // search left
+            }
+        }
     }
-    // Right half is sorted
-    else {
-      if (nums[mid] < target && target <= nums[right]) {
-        left = mid + 1; // search right
-      } else {
-        right = mid - 1; // search left
-      }
-    }
-  }
 
-  return -1; // not found
+    return -1; // not found
 }
