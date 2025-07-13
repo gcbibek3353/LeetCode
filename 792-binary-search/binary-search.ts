f32bc1012cb8c8b1581a516b9a9b816ddffcb078
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-13 07:41:26 +0530

    Time: 0 ms (100.00%) | Memory: 58.9 MB (33.88%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -1,6 +1,5 @@
 function search(nums: number[], target: number): number {
-    if (nums.length === 0) return -1;
-    if (nums.length === 1) return (nums[0] === target) ? 0 : -1;
+
     let l = 0;
     let r = nums.length - 1;
     while (l <= r) {
