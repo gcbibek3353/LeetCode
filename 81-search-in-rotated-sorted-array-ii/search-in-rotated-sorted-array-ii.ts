08d5b01409dbaab68501731cb46bf8c89b157c5b
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-14 08:03:43 +0530

    Time: 0 ms (100.00%) | Memory: 55.3 MB (78.29%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.ts
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.ts
@@ -1,52 +1,25 @@
 function search(nums: number[], target: number): boolean {
-    const { length } = nums;
-    let left = 0;
-    let right = length -1;
+    let n = nums.length;
+    let l = 0;
+    let r = n - 1;
 
-    while (left < right) {
-        const mid = Math.floor((right - left)/2)+left;
-
-        if (nums[left] === target) {
-            return true;
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        if (nums[m] === target) return true;
+        else if ((nums[m] === nums[l]) && (nums[m] === nums[r])) {
+            l++;
+            r--;
+            continue;
         }
-        if (nums[right] === target) {
-            return true;
+        else if (nums[l] <= nums[m]) {  // left is sorted
+            if (nums[l] <= target && nums[m] > target) r = m - 1;
+            else l = m + 1;
         }
-        if (nums[mid] === target) {
-            return true;
+        else {  // right is sorted
+            if (nums[r] >= target && nums[m] < target) l = m + 1;
+            else r = m - 1;
         }
-        if (nums[mid] === nums[right]) {
-            right = right -1;
-        } else if (nums[mid] > nums[right]) {
-            left = mid +1;
-        } else {
-            right = mid;
-        };
-    };
-
-    const start = left;
-    left = 0;
-    right = length -1;
-
-    if (target <= nums[right] && target >= nums[start]) {
-        left = start;
-    } else {
-        right = start -1;
     }
-
-    while (left <= right) {
-        const mid = Math.floor((right-left)/2)+left;
-
-        if (target === nums[mid]) {
-            return true;
-        }
-        if (target > nums[mid]) {
-            left = mid +1; 
-        }
-        if (target < nums[mid]) {
-            right = mid -1;
-        }
-    };
 
     return false;
 };
