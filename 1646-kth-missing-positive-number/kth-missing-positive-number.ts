ffe78936b2335e65b55d9820f117dadc0d36ea26
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-19 09:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (68.45%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.ts
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.ts
@@ -1,17 +1,14 @@
 function findKthPositive(arr: number[], k: number): number {
-   
     let l = 0;
     let r = arr.length - 1;
-
-    while( l <= r ) {
-        let m = Math.floor( ( l + r) / 2);
-        if((arr[m] - (m + 1)) < k) l = m + 1;
+    const getNoOfMissing = (i: number): number => (arr[i] - (i + 1));
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        const missing = getNoOfMissing(m);
+        if (missing < k) l = m + 1;
         else r = m - 1;
     }
-
-
     if (r === -1) return k;
+    return (arr[r] + k - getNoOfMissing(r));
 
-    return arr[r] + ( k - (arr[r] - (r + 1)));
-    //     7      +   5  - (7  -  4)
 };
