a7c58f0801f55c1c602bfa3e0092167f2bcff2ef
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-19 08:10:48 +0530

    Time: 20 ms (71.11%) | Memory: 65 MB (100.00%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.ts
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.ts
@@ -1,51 +1,34 @@
 function minDays(bloomDay: number[], m: number, k: number): number {
-  const n = bloomDay.length
+    if ((m * k) > bloomDay.length) return -1;
+    let l = Math.min(...bloomDay);
+    let r = Math.max(...bloomDay);
+    let ans = Number.MAX_SAFE_INTEGER;
 
-  // Not enough flowers to form the required bouquets
-  if (n < m * k) return -1
+    const isPossibleWithinGivenDays = (day: number): boolean => {
+        let curFlowers = 0;
+        let curBouquets = 0;
 
-  // Helper function to check if we can form m bouquets in given days
-  function canMakeBouquets(days: number): boolean {
-    let bouquets = 0
-    let flowers = 0
-
-    for (let i = 0; i < n; i++) {
-      // Checks if the flower at `ith` index has bloomed
-      if (bloomDay[i] <= days) {
-        flowers++
-        // If we have `k` flowers bloomed, we can make a bouquet
-        if (flowers == k) {
-          bouquets++
-          flowers = 0
+        for (let i = 0; i < bloomDay.length; i++) {
+            if (bloomDay[i] <= day) {
+                curFlowers++;
+                if (curFlowers >= k) {
+                    curBouquets++;
+                    curFlowers = 0;
+                }
+            } else {
+                curFlowers = 0;
+            }
         }
-      }
-      // Reset the count if the flower hasn't bloomed
-      else {
-        flowers = 0
-      }
-
-   
+        return curBouquets >= m;
     }
 
-    return bouquets >= m
-  }
-
-  // Initialize the limits of the binary search
-  let left = Math.min(...bloomDay)
-  let right = Math.max(...bloomDay)
-
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2)
-
-    // If we can make `m` bouquets in `mid` days, try for a smaller number of days
-    if (canMakeBouquets(mid)) {
-      right = mid
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2);
+        if (isPossibleWithinGivenDays(m)) {
+            ans = Math.min(ans, m);
+            r = m - 1;
+        }
+        else l = m + 1;
     }
-    // Otherwise, try for a larger number of days
-    else {
-      left = mid + 1
-    }
-  }
-
-  return left
-}
+    return ans;
+};
