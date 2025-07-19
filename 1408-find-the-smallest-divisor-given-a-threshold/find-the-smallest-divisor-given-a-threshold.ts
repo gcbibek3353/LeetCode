36a4a19046171daabf8dba79edac2ad8a5d0f0a5
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-19 08:21:11 +0530

    Time: 4 ms (98.51%) | Memory: 57.5 MB (89.55%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.ts
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.ts
@@ -1,24 +1,24 @@
 function smallestDivisor(nums: number[], threshold: number): number {
-    let l = 0;
-    let r = Math.max(...nums); // O(n)
-    let ans = Infinity;
-
-    const isValid = (m: number): boolean => {
+    let l = 1;
+    let r = Math.max(...nums);
+    let ans = Number.MAX_SAFE_INTEGER;
+    const getSumAfterDivide = (m : number) : number => {
         let curSum = 0;
-        for (let i = 0; i < nums.length; i++) {
-            let dividedAns = Math.ceil(nums[i] / m);
-            curSum += dividedAns;
+        for(let i = 0; i < nums.length; i ++){
+            curSum += Math.ceil(nums[i] / m);
         }
-        return curSum <= threshold;
+        return curSum;
     }
 
-    while (l <= r) {
-        let m = Math.floor((l + r) / 2);
-        if (isValid(m)) {
-            ans = Math.min(ans, m);
+    while(l <= r) {
+        let m = Math.floor( ( l + r) / 2);
+        let sum = getSumAfterDivide(m);
+        if(sum <= threshold) {
+            ans = Math.min(ans , m);
             r = m - 1;
         }
         else l = m + 1;
     }
+
     return ans;
 };
