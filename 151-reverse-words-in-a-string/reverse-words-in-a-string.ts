7409e976ea62c950f3815941188474594fa77b65
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-21 10:05:52 +0530

    Time: 2 ms (58.32%) | Memory: 58.1 MB (17.84%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -1,3 +1,13 @@
 function reverseWords(s: string): string {
-    return s.trim().split(' ').map(word => word.trim()).filter(word => word !== "").reverse().join(' ');
+  
+ let words = s.split(' ');
+    let res = [];
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        if (words[i]) {
+            res.push(words[i]);
+        }
+    }
+
+    return res.join(' ');
 };
