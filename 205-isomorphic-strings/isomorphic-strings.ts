343d909c3dc28f963e28c98dfd126d2cb7503e1f
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-21 10:31:19 +0530

    Time: 6 ms (70.26%) | Memory: 57.1 MB (53.22%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.ts
+++ 205-isomorphic-strings/isomorphic-strings.ts
@@ -1,16 +1,19 @@
 function isIsomorphic(s: string, t: string): boolean {
-    if(s.length !== t.length) return false;
-    const smap = new Map<string,string>();
-    const tmap = new Map<string,string>();
-    for(let i = 0; i < s.length; i ++){
-        if(
-            (smap.has(s[i]) && smap.get(s[i]) !== t[i]) ||
-            (tmap.has(t[i]) && tmap.get(t[i]) !== s[i])
-        ) return false;
-        else{
-            smap.set(s[i],t[i]);
-            tmap.set(t[i],s[i]);
-        } 
+    if (s.length !== t.length) return false;
+    let smp = new Map<string, string>();
+    let tmp = new Map<string, string>();
+
+    for (let i = 0; i < s.length; i++) {
+        if (smp.has(s.charAt(i))) {
+            if (smp.get(s.charAt(i)) !== t.charAt(i)) return false;
+        }
+        else if (tmp.has(t.charAt(i))) {
+            if(tmp.get(t.charAt(i)) !== s.charAt(i)) return false;
+        }
+        else {
+            smp.set(s.charAt(i), t.charAt(i));
+            tmp.set(t.charAt(i), s.charAt(i));
+        }
     }
     return true;
 };
