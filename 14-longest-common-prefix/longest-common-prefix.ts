b133f806fd60ada1b34163cc623b17f5230789fe
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-21 10:17:36 +0530

    Time: 3 ms (39.73%) | Memory: 56.2 MB (53.95%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,11 +1,12 @@
 function longestCommonPrefix(strs: string[]): string {
-  let commonPrefix = "";
-  for(let i = 0; i < strs[0].length; i ++){
-    let curChar = strs[0].charAt(i);
-    for(let j = 1; j < strs.length; j ++){
-        if(strs[j].charAt(i) != curChar) return commonPrefix;
+
+    let ans = "";
+
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[j].charAt(i) !== strs[0].charAt(i)) return ans;
+        }
+        ans += strs[0].charAt(i);
     }
-    commonPrefix += curChar;
-  }  
-  return commonPrefix;
+    return ans;
 };
