e7feefbc82b221c156ff396ff8a9c73f199319d9
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-29 16:29:20 +0530

    Time: 3 ms (22.77%) | Memory: 56.3 MB (29.21%) - LeetSync

---
+++ 216-combination-sum-iii/combination-sum-iii.ts
@@ -0,0 +1,17 @@
+function combinationSum3(k: number, n: number, limit = 9) {
+    const recur = (i: number, sum: number) => {
+        if (sum === n && backtrack.length === k) {
+            res.push(backtrack.slice());
+            return;
+        }
+        if (sum > n || backtrack.length > k || i > limit) return;
+        recur(i + 1, sum);
+        backtrack.push(i);
+        recur(i + 1, sum + i);
+        backtrack.pop();
+    };
+    const backtrack: number[] = [];
+    const res: number[][] = [];
+    recur(1, 0);
+    return res;
+}
