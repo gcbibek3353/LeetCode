c818341f11b9c70f50da71a1f2cba94a09b911a6
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-07-31 08:42:46 +0530

    Time: 0 ms (100.00%) | Memory: 55.2 MB (60.71%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.ts
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.ts
@@ -1,9 +1,9 @@
 function minBitFlips(start: number, goal: number): number {
-  let xorr = start ^ goal;
-  let count = 0;
-  for(let i = 0; i < 32; i ++){
-    let bitMask = 1 << i;
-    if((bitMask & xorr) != 0) count ++;
-  }
-  return count;  
+    let xorr = start ^ goal;
+    let count = 0;
+    for (let i = 0; i < 32; i++) {
+        let bitMask = 1 << i;
+        if ((bitMask & xorr )!== 0) count++;
+    }
+    return count;
 };
