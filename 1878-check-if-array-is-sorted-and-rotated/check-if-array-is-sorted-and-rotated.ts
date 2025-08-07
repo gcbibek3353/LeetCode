d8d39776f25f78071a5475b96032e8a97319a5f3
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-08-07 07:39:05 +0530

    Time: 0 ms (100.00%) | Memory: 55.6 MB (24.06%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.ts
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.ts
@@ -1,19 +1,14 @@
 function check(nums: number[]): boolean {
-    let minIndex = -1;
-
-    for (let i = nums.length - 2; i >= 0; i--) {
+    let dip = -1;
+    for (let i = 0; i < nums.length - 1; i++) {
         if (nums[i] > nums[i + 1]) {
-            minIndex = i + 1;
+            dip = i + 1;
+            break;
         }
     }
-    if (minIndex === -1) return true;
-
     for (let i = 0; i < nums.length - 1; i++) {
-        if (nums[(i + minIndex) % nums.length] > nums[(i + minIndex + 1) % nums.length]){
-            console.log(i , minIndex);
-        return false;
-        } 
+        let index = ((i + dip) % nums.length);
+        if (nums[index] > nums[(index + 1) % nums.length]) return false;
     }
-
     return true;
 };
