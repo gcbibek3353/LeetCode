9243fd056d995bacc0fd0c292056a429c7fab64b
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-10-12 09:28:14 +0530

    Time: 2 ms (55.33%) | Memory: 44.8 MB (88.11%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,16 +1,14 @@
 class Solution {
     public int maxProduct(int[] nums) {
-       if(nums.length == 0) return 0;
-       int overallMax = nums[0];
-       int maxSoFar = nums[0];
-       int minSoFar = nums[0];
-       for(int i = 1; i < nums.length;i ++) {
-        int curNum = nums[i];
-        int tempMax = maxSoFar;
-        maxSoFar = Math.max(curNum , Math.max(tempMax * curNum , minSoFar * curNum));
-        minSoFar = Math.min(curNum , Math.min(tempMax * curNum , minSoFar * curNum));
-        overallMax = Math.max(overallMax , maxSoFar);
-       }
-       return overallMax;
+      int highestPositive = nums[0];
+      int highestNegative = nums[0];
+      int ans = nums[0];
+      for(int i = 1; i < nums.length; i ++) {
+        int tempMax = highestPositive;
+        highestPositive = Math.max(Math.max(tempMax * nums[i] , highestNegative * nums[i]) , nums[i]);
+        highestNegative = Math.min(Math.min(tempMax * nums[i] , highestNegative * nums[i]) , nums[i]);
+        ans = Math.max(highestPositive , ans);
+      }
+      return ans;
     }
 }
