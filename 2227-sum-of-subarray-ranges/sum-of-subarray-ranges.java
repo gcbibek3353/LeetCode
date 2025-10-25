43e0d29800b5eb1230fdb20fe44195fef12215f8
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-10-25 12:01:14 +0530

    Time: 44 ms (9.26%) | Memory: 44.3 MB (68.81%) - LeetSync

---
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -0,0 +1,84 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        return subArrayMax(nums) - subArrayMin(nums);
+    }
+    private long subArrayMax(int[] arr) {
+          int n = arr.length;
+        int[] nge = getNGE(arr);
+        int[] pgee = getPGEE(arr);
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            int left = i - pgee[i];
+            int right = nge[i] - i;
+            long freq = 1L * left * right;
+            sum += freq * arr[i];
+        }
+        return sum;
+    }
+    private int[] getNGE(int[] arr) {
+         int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+    private int[] getPGEE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : -1;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    private long subArrayMin(int[] arr) {
+        int n = arr.length;
+        int[] nse = getNSE(arr);
+        int[] psee = getPSEE(arr);
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            int left = i - psee[i];
+            int right = nse[i] - i;
+            long freq = 1L * left * right;
+            sum += freq * arr[i];
+        }
+        return sum;
+    }
+    private int[] getNSE(int[] arr) {
+         int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+    private int[] getPSEE(int[] arr) {
+         int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : -1;
+            st.push(i);
+        }
+        return ans;
+    }
+}
