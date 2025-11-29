1b85bf381e954b726f6b25533900547207793b7a
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2025-11-29 09:44:10 +0530

    Time: 14 ms (47.95%) | Memory: 59.5 MB (19.80%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,19 +1,24 @@
 class Solution {
-    public int numberOfSubarrays(int[] nums, int goal) {
-        return noOfSubarraysLesserOrEqual(nums , goal) - noOfSubarraysLesserOrEqual(nums , goal - 1);
+    public int numberOfSubarrays(int[] nums, int k) {
+        return numSubArrayLesserOREqual(nums, k) - numSubArrayLesserOREqual(nums, k - 1);
     }
 
-      public int noOfSubarraysLesserOrEqual(int[] nums , int goal) {
-        if(goal < 0) return 0;
-        int i = 0;
-        int curSum = 0;
+    private int numSubArrayLesserOREqual(int[] nums, int goal) {
+        if (goal < 0)
+            return 0;
         int ans = 0;
-        for(int j = 0; j < nums.length; j ++) {
-            curSum += nums[j]%2;
-            while(curSum > goal && i <= j) curSum -= nums[i ++]%2;
-        ans += (j - i + 1);
+        int curSum = 0;
+        int l = 0;
+        for (int r = 0; r < nums.length; r++) {
+            // curSum += nums[r];
+            if(nums[r] % 2 == 1) curSum ++;
+            while (curSum > goal && l <= r){
+                // curSum -= nums[l++];
+                if(nums[l] % 2 == 1) curSum --;
+                l ++;
+            }
+            ans += (r - l + 1);
         }
         return ans;
     }
-    
 }
