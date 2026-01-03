d8dae8a70b0e49589708f3efaaed6939261cf1f6
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-01-03 09:35:44 +0530

    Time: 1 ms (54.16%) | Memory: 43 MB (49.70%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,36 +1,33 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
-        int ans = Integer.MAX_VALUE;
+        int ans = 0;
         int l = 0;
         int r = 0;
-        for (int num : nums) {
-            l = Math.max(l, num);
+        for(int num : nums) {
+            l = Math.max(num , l);
             r += num;
         }
 
-        while (l <= r) {
+        while(l <= r) {
             int m = l + (r - l) / 2;
-            int partitions = getPartition(nums, m);
-            if (partitions > k)
-                l = m + 1;
+            int partitions = getPartitions(nums ,m);
+            if(partitions > k) l = m + 1;
             else {
                 ans = m;
                 r = m - 1;
             }
         }
         return ans;
-
     }
-
-    private int getPartition(int[] arr, int maxSum) {
-         int partitions = 1;
+    private int getPartitions(int[] nums , int maxSum) {
+        int partitions = 1;
         int curSum = 0;
-
-        for (int num : arr) {
-            if (curSum + num > maxSum) {
-                partitions++;
+        for(int num : nums) {
+            if(curSum + num > maxSum) {
+                partitions ++;
                 curSum = num;
-            } else {
+            }
+            else {
                 curSum += num;
             }
         }
