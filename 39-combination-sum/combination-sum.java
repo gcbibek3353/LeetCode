95710bd318f0495e6c9781d29d15d0a10cc9366b
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-01-12 09:33:08 +0530

    Time: 3 ms (39.04%) | Memory: 45.7 MB (72.47%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,20 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> curArr = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> curArr = new ArrayList<>();
-        dfs(0,0,curArr,target , candidates);
+        dfs(candidates , 0 , 0 , target);
         return ans;
     }
-    private void dfs(int index , int curSum , List<Integer> curArr , int target , int[] candidates) {
-        if(curSum == target){
-            List<Integer> curAns = new ArrayList<>();
-            for(int i = 0; i < curArr.size(); i ++)curAns.add(curArr.get(i));
-            ans.add(curAns);
+
+    private void dfs(int[] candidates , int index , int curSum , int target) {
+        if(curSum == target) {
+            ans.add(new ArrayList<>(curArr));
             return;
         }
         if(curSum > target || index >= candidates.length) return;
-        dfs(index + 1 , curSum , curArr , target , candidates);
         curArr.add(candidates[index]);
-        dfs(index , curSum + candidates[index] , curArr , target , candidates);
+        dfs(candidates , index , curSum + candidates[index],target);
         curArr.remove(curArr.size() - 1);
+        dfs(candidates , index + 1 , curSum , target);
     }
 }
