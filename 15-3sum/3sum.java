a58a13f8b3f3b143fc548744deb31af2860909d2
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-02-10 18:53:12 +0530

    Time: 31 ms (80.33%) | Memory: 60.3 MB (12.58%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,29 @@
 class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (i == 0 || nums[i] != nums[i - 1]) {
-                twoSum(nums, i, ans);
-            }
+        for(int i = 0; i < nums.length; i ++) {
+            if(i == 0 || nums[i] != nums[i - 1])
+            twoSum(nums , i + 1 , nums.length - 1, nums[i]);
         }
         return ans;
     }
-
-    private void twoSum(int[] nums, int start, List<List<Integer>> ans) {
-        int target = -nums[start];
-        int l = start + 1;
-        int r = nums.length - 1;
-        while (l < r) {
-            if (nums[l] + nums[r] == target) {
+    private void twoSum(int[] nums , int l ,int r ,int target) {
+        while(l < r) {
+            int curSum = nums[l] + nums[r];
+            if(curSum == -target) {
                 List<Integer> curAns = new ArrayList<>();
-                curAns.add(nums[start]);
                 curAns.add(nums[l]);
                 curAns.add(nums[r]);
+                curAns.add(target);
                 ans.add(curAns);
-                l++;
+                l ++;
                 r --;
-                while (l < r && nums[l] == nums[l - 1])
-                    l++;
-                while (l < r && nums[r] == nums[r + 1])
-                    r--;
-            } else if (nums[l] + nums[r] < target)
-                l++;
-            else
-                r--;
+                while(l < r && nums[l] == nums[l - 1]) l ++;
+                while(l < r && nums[r] == nums[r + 1]) r --;
+            }
+            else if (curSum < -target) l ++;
+            else r --;
         }
     }
 }
