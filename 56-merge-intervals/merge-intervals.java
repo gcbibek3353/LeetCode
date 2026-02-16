70136276cc0e38392c8dc9e41de4cef9178e88a9
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-02-16 09:44:17 +0530

    Time: 8 ms (90.41%) | Memory: 49.5 MB (5.48%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length <= 1) return intervals;
-        // Sorting is important for case like : [[4,7],[1,4]] -> [[1,7]]
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> ans = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         ans.add(intervals[0]);
-        for(int i = 1; i < intervals.length; i ++) {
-            int[] lastMerged = ans.get(ans.size() - 1);
-            int lastOfFirst = lastMerged[1];
+        for(int i = 1; i < intervals.length; i ++){
+            int[] lastInterval = ans.get(ans.size() - 1);
+            int lastOfFirst = lastInterval[1];
             int firstOfLast = intervals[i][0];
             if(lastOfFirst >= firstOfLast){
-            lastMerged[1] = Math.max(intervals[i][1],lastMerged[1]);
-            // ans[ans.length - 1][1] = intervals[i][1];
-            } 
-            else ans.add(intervals[i]);
+                lastInterval[1] = Math.max(lastInterval[1],intervals[i][1]);
+            }
+            else {
+                ans.add(intervals[i]);
+            }
         }
         return ans.toArray(new int[ans.size()][]);
+
     }
 }
