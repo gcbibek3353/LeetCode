12fcb4a5d50e7b129067bb75fe8930bd3ed7976c
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-02-17 09:17:22 +0530

    Time: 2 ms (6.27%) | Memory: 46.6 MB (37.22%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -1,27 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode();
-        ListNode res = dummy;
-        int total = 0, carry = 0;
-
-        while (l1 != null || l2 != null || carry != 0) {
-            total = carry;
-
-            if (l1 != null) {
-                total += l1.val;
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                total += l2.val;
-                l2 = l2.next;
-            }
-
-            int num = total % 10;
-            carry = total / 10;
-            dummy.next = new ListNode(num);
-            dummy = dummy.next;
+        ListNode head = new ListNode();
+        ListNode curNode = head;
+        int carry = 0;
+        while(l1 != null || l2 != null) {
+            int l1Val = (l1 != null) ? l1.val : 0;
+            int l2Val = (l2 != null) ? l2.val : 0;
+            int sum = l1Val + l2Val + carry;
+            int val = sum % 10;
+            carry = sum / 10;
+            ListNode newNode = new ListNode();
+            newNode.val = val;
+            curNode.next = newNode;
+            curNode = curNode.next;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
         }
-
-        return res.next;        
+        if(carry != 0){
+            ListNode newNode = new ListNode();
+            newNode.val = carry;
+            curNode.next = newNode;
+        }
+        return head.next;
     }
 }
