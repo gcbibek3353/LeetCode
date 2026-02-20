57ed8ee720340f214acbdf78832718f0b6ccc503
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-02-20 15:58:55 +0530

    Time: 6 ms (7.23%) | Memory: 78.4 MB (5.00%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length == 0 || inorder.length == 0) return null;
+
+        TreeNode root = new TreeNode(preorder[0]);
+        int mid = findIndex(inorder , root.val);
+        root.left = buildTree(Arrays.copyOfRange(preorder , 1 , mid + 1) , Arrays.copyOfRange(inorder, 0 , mid));
+        root.right = buildTree(Arrays.copyOfRange(preorder,mid + 1,preorder.length) , Arrays.copyOfRange(inorder , mid + 1 , inorder.length));
+        return root;
+    }
+
+    public static int findIndex(int[] arr, int target) {
+    for (int i = 0; i < arr.length; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index if the element is found
+        }
+    }
+    return -1; // Return -1 if the element is not found
+}
+}
