5646e7b0f4e667ed565d29d76521bdc941dc74f7
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-07 13:07:16 +0530

    Time: 7 ms (14.84%) | Memory: 50.9 MB (99.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,40 +1,55 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int islands = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Set<String> visited = new HashSet<>();
 
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (grid[r][c] == '1' && !visited.contains(r + "," + c)) {
-                    islands++;
-                    bfs(grid, r, c, visited, directions, rows, cols);
-                }
-            }
+    static class coords{
+        int i;
+        int j;
+        coords(int i,int j){
+            this.i = i;
+            this.j = j;
         }
-
-        return islands;        
     }
 
-    private void bfs(char[][] grid, int r, int c, Set<String> visited, int[][] directions, int rows, int cols) {
-        Queue<int[]> q = new LinkedList<>();
-        visited.add(r + "," + c);
-        q.add(new int[]{r, c});
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int r = grid.length;
+        int c = grid[0].length;
+        boolean[][] visited = new boolean[r][c];
+        for(int i = 0; i < r; i ++){
+            boolean[] curRow = new boolean[c];
+            for(int j = 0; j < c; j ++){
+                curRow[j] = false;
+            }
+            visited[i] = curRow;
+        }
 
-        while (!q.isEmpty()) {
-            int[] point = q.poll();
-            int row = point[0], col = point[1];
+        for(int i = 0; i < r; i ++){
+            for(int j = 0; j < c; j ++){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    bfs(grid,i,j,visited);
+                    count ++;
+                } 
+            }
+        }
+        return count;
+    }
 
-            for (int[] direction : directions) {
-                int nr = row + direction[0], nc = col + direction[1];
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] == '1' && !visited.contains(nr + "," + nc)) {
-                    q.add(new int[]{nr, nc});
-                    visited.add(nr + "," + nc);
+    private static void bfs(char[][] grid , int i, int j,boolean[][] visited){
+        Queue<coords> q = new LinkedList<>();
+        q.offer(new coords(i,j));
+        int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!q.isEmpty()) {
+            coords curCoords = q.poll();
+            for(int[] direction : directions){
+                int curRow = curCoords.i + direction[0];
+                int curCol = curCoords.j + direction[1];
+                if(curRow < 0 || curRow >= grid.length || curCol < 0 || curCol >= grid[0].length) continue;
+                if(grid[curRow][curCol] == '1' && !visited[curRow][curCol]){
+                    q.offer(new coords(curRow,curCol));
+                    visited[curRow][curCol] = true;
                 }
             }
         }
-    }    
+    }
+
 }
