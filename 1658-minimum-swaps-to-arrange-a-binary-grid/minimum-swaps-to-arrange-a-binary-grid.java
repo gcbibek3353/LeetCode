1cf81b14622f6fdadd377a7cd266109625393b88
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-08 14:40:24 +0530

    Time: 2 ms (96.19%) | Memory: 49.7 MB (52.83%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.java
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.java
@@ -1,33 +1,33 @@
 class Solution {
     public int minSwaps(int[][] grid) {
         int n = grid.length;
-        int[] zerosCount = new int[n];
+        int[] trailingZerosCount = new int[n];
         int steps = 0;
 
-        for(int i = 0; i < n; i ++){
-            int curCount = 0;
-            int j = n - 1;
-            while(j > 0){
-                if(grid[i][j --] == 0) curCount ++;
-                else break;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = n - 1; j >= 0; j--) {
+                if (grid[i][j] == 0)
+                    count++;
+                else
+                    break;
             }
-            zerosCount[i] = curCount;
+            trailingZerosCount[i] = count;
         }
 
         for(int i = 0; i < n; i ++){
-            int needed = n - i - 1;
+            int neededZeros = n - i - 1;
             int j = i;
-            while(j < n) {
-                if(zerosCount[j] >= needed) break;
+            while(j < n){
+                if(neededZeros <= trailingZerosCount[j]) break;
                 j ++;
             }
             if(j == n) return -1;
             steps += j - i;
-
-            while(j > i) {
-                int temp = zerosCount[j];
-                zerosCount[j] = zerosCount[j - 1];
-                zerosCount[j - 1] = temp;
+            while( j > i){
+                int temp = trailingZerosCount[j];
+                trailingZerosCount[j] = trailingZerosCount[j - 1];
+                trailingZerosCount[j - 1] = temp;
                 j --;
             }
         }
