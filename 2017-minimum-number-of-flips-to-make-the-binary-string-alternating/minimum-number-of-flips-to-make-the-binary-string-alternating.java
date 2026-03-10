e5fe01968d1c0295ad00f8134b9efbe0be75b9b5
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-10 16:55:11 +0530

    Time: 31 ms (24.98%) | Memory: 47.6 MB (22.79%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,33 +1,29 @@
 class Solution {
-
     public int minFlips(String s) {
         int n = s.length();
-        int[][] pre = new int[n][2];
+        String doubled = s + s;
 
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            // Compute the prefix array
-            pre[i][0] = (i == 0 ? 0 : pre[i - 1][1]) + (ch == '1' ? 1 : 0);
-            pre[i][1] = (i == 0 ? 0 : pre[i - 1][0]) + (ch == '0' ? 1 : 0);
-        }
+        int diff1 = 0, diff2 = 0;
+        int ans = Integer.MAX_VALUE;
 
-        int ans = Math.min(pre[n - 1][0], pre[n - 1][1]);
-        // If the length is odd, consider the move operation
-        if (n % 2 == 1) {
-            int[][] suf = new int[n][2];
-            for (int i = n - 1; i >= 0; i--) {
-                char ch = s.charAt(i);
-                // Suffix Array Construction
-                suf[i][0] =
-                    (i == n - 1 ? 0 : suf[i + 1][1]) + (ch == '1' ? 1 : 0);
-                suf[i][1] =
-                    (i == n - 1 ? 0 : suf[i + 1][0]) + (ch == '0' ? 1 : 0);
+        for (int i = 0; i < doubled.length(); i++) {
+
+            char expected1 = (i % 2 == 0) ? '0' : '1';
+            char expected2 = (i % 2 == 0) ? '1' : '0';
+
+            if (doubled.charAt(i) != expected1) diff1++;
+            if (doubled.charAt(i) != expected2) diff2++;
+
+            if (i >= n) {
+                char prevExpected1 = ((i - n) % 2 == 0) ? '0' : '1';
+                char prevExpected2 = ((i - n) % 2 == 0) ? '1' : '0';
+
+                if (doubled.charAt(i - n) != prevExpected1) diff1--;
+                if (doubled.charAt(i - n) != prevExpected2) diff2--;
             }
 
-            // Traverse all split points
-            for (int i = 0; i < n - 1; i++) {
-                ans = Math.min(ans, pre[i][0] + suf[i + 1][0]);
-                ans = Math.min(ans, pre[i][1] + suf[i + 1][1]);
+            if (i >= n - 1) {
+                ans = Math.min(ans, Math.min(diff1, diff2));
             }
         }
 
