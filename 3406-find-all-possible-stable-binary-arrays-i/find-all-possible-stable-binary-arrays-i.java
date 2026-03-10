daaf6cfa03fdf6dda663c15a90486088d96934bf
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-10 17:42:08 +0530

    Time: 251 ms (38.16%) | Memory: 49.1 MB (21.05%) - LeetSync

--- 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.java
@@ -1,37 +1,31 @@
 class Solution {
-    private Integer[][][] dp;
-    private static final int MOD = 1_000_000_007;
-
+    Integer[][][] dp;
+    int MOD = 1_000_000_007;
     public int numberOfStableArrays(int zero, int one, int limit) {
         dp = new Integer[zero + 1][one + 1][2];
-        int countStartingWithZero = solve(zero , one , true , limit);
-        int countStartingWithOne = solve(zero , one , false , limit);
-        return (countStartingWithZero + countStartingWithOne) % MOD;
+        int startingWithZero = count(zero , one , false,limit);
+        int startingWithOne = count(zero , one , true , limit);
+        return (startingWithZero + startingWithOne) % MOD;
     }
-    private int solve(int zerosLeft , int onesLeft , boolean lastWasOne , int limit) {
-         if(onesLeft == 0 && zerosLeft == 0) return 1;
 
-        int last = lastWasOne ? 1 : 0;
-        if(dp[zerosLeft][onesLeft][last] != null) {
-            return dp[zerosLeft][onesLeft][last];
-        }
+    private int count(int zeroLeft , int oneLeft , boolean onesTurn , int limit) {
+        if(zeroLeft == 0 && oneLeft == 0) return 1;
+        int correspondingDigit = onesTurn ? 1 : 0;
+        if(dp[zeroLeft][oneLeft][correspondingDigit] != null) return dp[zeroLeft][oneLeft][correspondingDigit];
 
         long result = 0;
-
-        if(lastWasOne){
-            for(int len = 1; len <= Math.min(zerosLeft , limit); len++){
-                result += solve(zerosLeft - len , onesLeft , false , limit);
-                result %= MOD;
+        if(onesTurn){
+            for(int i = 1; i <= Math.min(limit , oneLeft); i ++){   
+                result += count(zeroLeft , oneLeft - i , false , limit);
             }
         }
-        else{
-            for(int len = 1; len <= Math.min(onesLeft , limit); len++){
-                result += solve(zerosLeft , onesLeft - len , true , limit); // fixed
-                result %= MOD;
+        else {
+            for(int i = 1; i <= Math.min(limit , zeroLeft); i ++){   
+                result += count(zeroLeft - i , oneLeft , true , limit);
             }
         }
-
-        dp[zerosLeft][onesLeft][last] = (int) result;
-        return dp[zerosLeft][onesLeft][last];
+        result = result % MOD;
+        dp[zeroLeft][oneLeft][correspondingDigit] = (int)result;
+        return dp[zeroLeft][oneLeft][correspondingDigit];
     }
 }
