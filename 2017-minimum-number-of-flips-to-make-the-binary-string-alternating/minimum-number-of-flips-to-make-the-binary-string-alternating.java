ad235a736c4991d7be7b725172439a65e2d8dd3c
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-11 09:30:38 +0530

    Time: 20 ms (69.62%) | Memory: 47.3 MB (52.28%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -3,27 +3,23 @@ class Solution {
         int n = s.length();
         String doubled = s + s;
 
-        int diff1 = 0, diff2 = 0;
+        int diff1 = 0;
         int ans = Integer.MAX_VALUE;
 
         for (int i = 0; i < doubled.length(); i++) {
 
             char expected1 = (i % 2 == 0) ? '0' : '1';
-            char expected2 = (i % 2 == 0) ? '1' : '0';
 
             if (doubled.charAt(i) != expected1) diff1++;
-            if (doubled.charAt(i) != expected2) diff2++;
 
             if (i >= n) {
                 char prevExpected1 = ((i - n) % 2 == 0) ? '0' : '1';
-                char prevExpected2 = ((i - n) % 2 == 0) ? '1' : '0';
 
                 if (doubled.charAt(i - n) != prevExpected1) diff1--;
-                if (doubled.charAt(i - n) != prevExpected2) diff2--;
             }
 
             if (i >= n - 1) {
-                ans = Math.min(ans, Math.min(diff1, diff2));
+                ans = Math.min(ans, Math.min(diff1, n - diff1));
             }
         }
 
