777f03afb801639901af9ceb63598c4fdc11f5a3
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-11 09:53:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.82%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -1,8 +1,14 @@
 class Solution {
     public int bitwiseComplement(int n) {
-        if (n == 0) return 1;
-        int bits = 32 - Integer.numberOfLeadingZeros(n);
-        int mask = (1 << bits) - 1;
+        if(n == 0) return 1;
+        if(n == 1) return 0;
+        int digitCount = 0;
+        for(int i = 0; i < 32; i ++){
+            int mask = 1 << i;
+            if((mask & n) != 0) digitCount = i + 1;
+        }
+
+        int mask = (1 << digitCount) - 1;
         return ~n & mask;
     }
 }
