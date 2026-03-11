ff038fe50d6e5fa8dfbd73b8d84f17956c3a5854
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-11 09:19:19 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (53.17%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,13 +1,12 @@
 class Solution {
     public char findKthBit(int n, int k) {
         if(n == 1) return '0';
-        int power = (int)Math.pow(2 , n);
-        int len = power - 1;
+        int len = (1 << n) - 1;
         int mid = (len / 2) + 1;
 
-        if(k == mid) return '1';
-        else if(k < mid) return findKthBit(n - 1 , k);
-        char c = findKthBit(n - 1 , len - k + 1);
-        return (c == '0') ? '1' : '0';
+        if(mid == k) return '1';
+        else if (k < mid) return findKthBit(n - 1 , k);
+        char char1 = findKthBit(n - 1 , len - k + 1);
+        return char1 == '0' ? '1' : '0';
     }
 }
