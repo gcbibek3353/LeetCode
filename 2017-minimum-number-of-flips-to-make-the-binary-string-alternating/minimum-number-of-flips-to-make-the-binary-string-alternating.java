5b5acc1f624b356b5b62f20121cd409a40aebeb7
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-20 22:48:44 +0530

    Time: 21 ms (66.24%) | Memory: 47.1 MB (67.31%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -2,27 +2,25 @@ class Solution {
     public int minFlips(String s) {
         int n = s.length();
         String doubled = s + s;
-
-        int diff1 = 0;
+        int differenceCount = 0;
         int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < doubled.length(); i ++) {
+            char expectedChar = (i % 2 == 0) ? '0' : '1';
+            char curChar = doubled.charAt(i);
+            if(expectedChar != curChar) differenceCount ++;
 
-        for (int i = 0; i < doubled.length(); i++) {
-
-            char expected1 = (i % 2 == 0) ? '0' : '1';
-
-            if (doubled.charAt(i) != expected1) diff1++;
-
-            if (i >= n) {
-                char prevExpected1 = ((i - n) % 2 == 0) ? '0' : '1';
-
-                if (doubled.charAt(i - n) != prevExpected1) diff1--;
+            if(i >= n){
+                char previouslyExpectedChar = ((i - n) % 2 == 0) ? '0' : '1';
+                char previousActualChar = doubled.charAt(i - n);
+                if(previouslyExpectedChar != previousActualChar) differenceCount --;
             }
 
-            if (i >= n - 1) {
-                ans = Math.min(ans, Math.min(diff1, n - diff1));
+            if(i >= n - 1){
+                int minDifference = Math.min(differenceCount , n - differenceCount);
+                ans = Math.min(ans , minDifference);
             }
+
         }
-
         return ans;
     }
 }
