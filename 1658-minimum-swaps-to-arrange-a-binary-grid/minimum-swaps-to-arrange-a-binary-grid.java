0887bc23b7b62247c7f974e09c0f4ba5f6e189ac
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-20 21:15:08 +0530

    Time: 2 ms (96.23%) | Memory: 49.7 MB (33.45%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.java
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.java
@@ -1,35 +1,35 @@
 class Solution {
     public int minSwaps(int[][] grid) {
-        int n = grid.length;
-        int swaps = 0;
-        int[] trailingZeros = new int[n];
+        int row = grid.length;
+        int[] trailingZerosCount = new int[row];
+        int steps = 0;
 
-        for(int i = 0; i < n; i ++){
+        for(int i = 0; i < grid.length; i ++){
             int count = 0;
-            int j = n - 1;
-            while(j >= 0){
+            for(int j = row - 1; j >= 0; j --){
                 if(grid[i][j] == 0) count ++;
                 else break;
-                j --;
             }
-            trailingZeros[i] = count;
+            trailingZerosCount[i] = count;
         }
-        for(int i = 0; i < n; i ++){
-            int needed = n - i - 1;
-            int j = i;
-            while(j < n && needed > trailingZeros[j]){
+        
+        for(int r = 0; r < grid.length; r ++){
+            int requiredZeros = row - r - 1;
+
+            int j = r;
+            while(j < row && requiredZeros > trailingZerosCount[j]){
                 j ++;
             }
-            if(j == n) return -1;
-            swaps += j - i;
-            while(j > i){
-                int temp = trailingZeros[j];
-                trailingZeros[j] = trailingZeros[j - 1];
-                trailingZeros[j - 1] = temp;
+            if(j == row) return -1;
+            steps += j - r;
+
+            while(j > r){
+                int temp = trailingZerosCount[j];
+                trailingZerosCount[j] = trailingZerosCount[j - 1];
+                trailingZerosCount[j - 1] = temp;
                 j --;
             }
         }
-        return swaps;
-
+        return steps;
     }
 }
