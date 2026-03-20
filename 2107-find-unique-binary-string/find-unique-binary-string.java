5ab07e4b2ca171c943456280c0a92024b0208ca5
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-20 22:58:23 +0530

    Time: 3 ms (43.25%) | Memory: 43.8 MB (21.53%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.java
+++ 2107-find-unique-binary-string/find-unique-binary-string.java
@@ -1,17 +1,21 @@
 class Solution {
+    int n;
+    Set<String> st;
     public String findDifferentBinaryString(String[] nums) {
-        Set<String> st = new HashSet<>();
-        int n = nums.length;
-        for(int i = 0; i < nums.length; i ++) {
-            st.add(nums[i]);
-        }
-        return generate("",st,n);
+        n = nums.length;
+        st = new HashSet<>();
+        for(String num : nums) st.add(num);
+        return generate("");
     }
 
-    protected String generate(String s , Set<String> st , int n) {
-        if(s.length() == n) return (st.contains(s) ? "" : s);
-        String zeroAddedAns = generate(s + '0', st , n);
-        if(zeroAddedAns.length() >= 1) return zeroAddedAns;
-        return generate(s + '1', st , n);
+    protected String generate(String num){
+        if(num.length() == n){
+            if(!st.contains(num)) return num;
+            else return "";
+        }
+        String zeroAddedAns = generate("0" + num);
+        if(zeroAddedAns != "") return zeroAddedAns;
+        String oneAddedAns = generate("1" + num);
+        return oneAddedAns;
     }
 }
