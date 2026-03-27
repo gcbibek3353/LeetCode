a20c98905746be40c55a0573b9d45c721d12ac49
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-03-27 11:32:55 +0530

    Time: 34 ms (5.74%) | Memory: 51.9 MB (91.64%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.java
@@ -1,26 +1,36 @@
 class KthLargest {
-
-    PriorityQueue<Integer> minHeap;
+    List<Integer> scores;
     int k;
-
     public KthLargest(int k, int[] nums) {
-        minHeap = new PriorityQueue<>();
+        this.scores = new ArrayList<>();
         this.k = k;
-
-        for (int num : nums) {
-            add(num);
+        for(int num : nums){
+            scores.add(num);
         }
+        Collections.sort(scores);
+    }
+    
+    public int add(int val) {
+        int index = findIndex( val);
+        scores.add(index , val);
+        return scores.get(scores.size() - k);
     }
 
-    public int add(int val) {
-        // Add to our minHeap if we haven't processed k elements yet
-        // or if val is greater than the top element (the k-th largest)
-        if (minHeap.size() < k || minHeap.peek() < val) {
-            minHeap.add(val);
-            if (minHeap.size() > k) {
-                minHeap.remove();
-            }
+    private int findIndex(int val) {
+        int l = 0;
+        int r = scores.size() - 1;
+        while(l <= r){
+            int m = l + (r - l) / 2;
+            if(scores.get(m) == val) return m;
+            else if(scores.get(m) < val) l = m + 1;
+            else r = m - 1;
         }
-        return minHeap.peek();
+        return l;
     }
 }
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
