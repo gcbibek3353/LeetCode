61d367b24d32a81ac2612b8d4951449df2055b77
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-06-14 22:21:35 +0530

    Time: 6 ms (5.62%) | Memory: 44.6 MB (56.19%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,17 +1,17 @@
 class Solution {
     List<String> ans = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        generate("(", 1, n);
+        generate("(" , 1 , n);
         return ans;
     }
 
-    private void generate(String curStr, int openCount , int n) {
+    private void generate(String curStr , int openCount , int n){
         int m = curStr.length();
         if(m == n * 2){
             ans.add(curStr);
             return;
         }
-        int closedCount = m - openCount;
+        int closedCount = curStr.length() - openCount;
         if(openCount < n) generate(curStr + "(" , openCount + 1 , n);
         if(closedCount < n && closedCount < openCount) generate(curStr + ")" , openCount , n);
     }
