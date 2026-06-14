ce422e73996f32ccaac89bf2650121fa1a94aad3
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-06-14 22:06:44 +0530

    Time: 7 ms (36.46%) | Memory: 46.7 MB (7.88%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,30 +1,25 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
-
-        for (String token : tokens) {
-
-            if (token.equals("+") ||
-                token.equals("-") ||
-                token.equals("*") ||
-                token.equals("/")) {
-
-                int b = stack.pop();
-                int a = stack.pop();
-
-                int result = evaluate(a, b, token);
-                stack.push(result);
-
-            } else {
-                stack.push(Integer.parseInt(token));
-            }
+        Deque<String> stack = new ArrayDeque<>();
+        for(String curToken : tokens) {
+            if(
+                curToken.equals("+") ||
+                curToken.equals("-") ||
+                curToken.equals("*") ||
+                curToken.equals("/") 
+                ){
+                    int secondOperand = Integer.parseInt(stack.poll());
+                    int firstOperand = Integer.parseInt(stack.poll());
+                    int ans = evaluate(firstOperand , secondOperand , curToken);
+                    stack.push(ans + "");
+                }
+                else stack.push(curToken);
         }
-
-        return stack.pop();
+        return Integer.parseInt(stack.poll());
     }
 
-    private int evaluate(int a, int b, String operator) {
-        switch (operator) {
+    private int evaluate(int a , int b , String operator){
+        switch(operator){
             case "+": return a + b;
             case "-": return a - b;
             case "*": return a * b;
