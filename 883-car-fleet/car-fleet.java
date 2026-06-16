bf106b23000063ae74e0935c7e5140d2ff504965
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-06-16 23:10:50 +0530

    Time: 85 ms (49.32%) | Memory: 101.8 MB (35.19%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+
+        int[][] cars = new int[n][2];
+        for(int i = 0;i<n;i++){
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+
+        Arrays.sort(cars,(a,b) -> Integer.compare(b[0],a[0]));
+
+        Stack<Double> st = new Stack<>();
+
+        for(int []car : cars){
+            int pos = car[0];
+            int spd = car[1];
+            double timeTaken = (double)(target - pos) / spd;
+
+            if(st.isEmpty() || timeTaken > st.peek()){
+                st.push(timeTaken);
+            }
+        }
+
+         return st.size();
+    }
+}
