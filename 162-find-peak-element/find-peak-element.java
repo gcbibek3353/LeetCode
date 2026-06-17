f97ec0ba04cb01da7a2444845655308f60faadf9
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-06-17 22:56:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (54.86%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,26 +1,23 @@
 class Solution {
-    public int findPeakElement(int[] arr) {
-        int n = arr.length;
-        if (n == 1)
-            return 0;
-        if (arr[0] > arr[1])
-            return 0;
-        if (arr[n - 1] > arr[n - 2])
-            return n - 1;
+    public int findPeakElement(int[] nums) {
+        if(nums.length == 1) return 0;
         int l = 0;
-        int r = arr.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (mid == 0)
-                mid = 1; // This will only execute when l = 0 and r = 1 , mid = 0 . 
-            boolean isIncreasingRight = (arr[mid] > arr[mid - 1]);
-            boolean isDecreasingLeft = (arr[mid] > arr[mid + 1]);
-            if (isIncreasingRight && isDecreasingLeft)
-                return mid;
-            else if (isIncreasingRight && !isDecreasingLeft)
-                l = mid + 1;
-            else
-                r = mid - 1;
+        int r = nums.length - 1;
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            if(m == 0){
+                if(nums[m] > nums[m + 1]) return 0;
+                else l = m + 1;
+            }
+            else if( m == nums.length - 1){
+                if(nums[m] > nums[m - 1]) return m;
+                else r = m - 1;
+            }
+            else{
+                if(nums[m] > nums[m - 1] && nums[m] > nums[m + 1]) return m;
+                else if (nums[m] > nums[m - 1] && nums[m] < nums[m + 1]) l = m + 1;
+                else r = m - 1;
+            }
         }
         return -1;
     }
