cb2690b924601665c36b08d730ee2e49bea59f0d
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-06-17 23:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (69.84%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,24 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0) return new int[]{-1,-1};
-        int[] ans = new int[2];
+        int firstOccurance = findFirstOrLastOccurance(nums , target , true);
+        int lastOccurance = findFirstOrLastOccurance(nums , target , false);
+        return new int[]{firstOccurance,lastOccurance};
+    }
+
+    private int findFirstOrLastOccurance(int[] nums , int target, boolean isFindingFirstOccurance){
         int l = 0;
         int r = nums.length - 1;
-        int startIndex = -1;
-        int endIndex = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] > target)
-                r = mid - 1;
-            else if (nums[mid] == target) {
-                endIndex = mid;
-                l = mid + 1;
-            } else
-                l = mid + 1;
+        int ans = -1;
+        while(l <= r) {
+            int m = l + (r - l) / 2;
+            if(nums[m] < target) l = m + 1;
+            else if (nums[m] == target){
+                ans = m;
+                if(isFindingFirstOccurance) r = m - 1;
+                else l = m + 1;
+            }
+            else r = m - 1;
         }
-        l = 0;
-        r = nums.length - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] > target)
-                r = mid - 1;
-            else if (nums[mid] == target) {
-                startIndex = mid;
-                r = mid - 1;
-            } else
-                l = mid + 1;
-        }
-
-        ans[0] = startIndex;
-        ans[1] = endIndex;
         return ans;
     }
 }
