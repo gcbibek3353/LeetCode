5c2f91a98e582f2007daf4c40087c3bff940b255
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-06-18 08:14:15 +0530

    Time: 1 ms (100.00%) | Memory: 48.4 MB (97.72%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,36 +1,44 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+
+        if(nums1.length > nums2.length)
+            return findMedianSortedArrays(nums2, nums1);
+
         int n1 = nums1.length;
         int n2 = nums2.length;
-        if(n1 > n2) return findMedianSortedArrays(nums2,nums1);
+
         int total = n1 + n2;
-        int leftSize = (total + 1) / 2;
-        int l = 0;
-        int r = n1;
-        int l1 = Integer.MIN_VALUE;
-        int l2 = Integer.MIN_VALUE;
-        int r1 = Integer.MAX_VALUE;
-        int r2 = Integer.MAX_VALUE;
-        while(l <= r) {
-            int cut1 = l + (r - l) /2 ;
-            int cut2 = leftSize - cut1;
+        int left = (total + 1) / 2;
 
-            l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+        int low = 0;
+        int high = n1;
 
-            l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-            r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+        while(low <= high){
 
-            if(l1 > r2) r = cut1 - 1;
-            else if(l2 > r1) l = cut1 + 1;
-            else break; 
+            int cut1 = (low + high) / 2;
+            int cut2 = left - cut1;
+
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1-1];
+            int r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2-1];
+            int r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if(l1 <= r2 && l2 <= r1){
+
+                if(total % 2 == 1)
+                    return Math.max(l1,l2);
+
+                return (Math.max(l1,l2) + Math.min(r1,r2)) / 2.0;
+            }
+
+            else if(l1 > r2)
+                high = cut1 - 1;
+
+            else
+                low = cut1 + 1;
         }
 
-        if(total % 2 == 1) {
-            return Math.max(l1 , l2);
-        }
-        else {
-            return (Math.max(l1,l2) + Math.min(r1,r2)) / 2.0;
-        }
+        return 0.0;
     }
 }
