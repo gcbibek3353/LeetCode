49ef2eba1f7941ac1aa109980c97f88c2012eca2
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-06-23 22:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (58.91%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,34 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null) return true;
-        int leftChildDepth = findDepth(root.left);
-        int rightChildDepth = findDepth(root.right);
-        boolean curNodeBalanced = false;
-        if(Math.abs(leftChildDepth - rightChildDepth) <= 1) curNodeBalanced = true;
-        boolean leftNodeBalanced = isBalanced(root.left);
-        boolean rightNodeBalanced = isBalanced(root.right);
-        return (leftNodeBalanced && rightNodeBalanced) && curNodeBalanced;
+        return dfsHeight(root) != -1;
     }
 
-    private int findDepth(TreeNode root){
-        if(root == null) return 0;
-        int leftDepth = findDepth(root.left);
-        int rightDepth = findDepth(root.right);
-        return 1 + Math.max(leftDepth , rightDepth);
+    private int dfsHeight(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftHeight = dfsHeight(node.left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = dfsHeight(node.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return 1 + Math.max(leftHeight, rightHeight);
     }
 }
