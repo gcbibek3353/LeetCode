0f3f9801b177e29c36e71dd05615c0e94fcaf1ab
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-07-07 10:15:07 +0530

    Time: 19 ms (97.87%) | Memory: 56.8 MB (28.71%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>(
+            (a, b) -> Integer.compare(
+                a[0] * a[0] + a[1] * a[1],
+                b[0] * b[0] + b[1] * b[1]
+            )
+        );
+
+        for (int[] point : points) {
+            minHeap.offer(point);
+        }
+
+        int[][] ans = new int[k][];
+
+        for (int i = 0; i < k; i++) {
+            ans[i] = minHeap.poll();
+        }
+
+        return ans;
+    }
+}
