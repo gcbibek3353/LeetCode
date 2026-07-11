f76e31d2a6fc006404b82124c7233b9108156921
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-07-11 18:46:36 +0530

    Time: 11 ms (4.31%) | Memory: 119.7 MB (9.88%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -1,30 +1,23 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        
         int totalGas = 0;
         int totalCost = 0;
-
-        for (int i = 0; i < gas.length; i++) {
+        for(int i = 0; i < gas.length; i ++){
             totalGas += gas[i];
             totalCost += cost[i];
         }
+        if(totalGas < totalCost) return -1;
 
-        // Global feasibility check
-        if (totalGas < totalCost) return -1;
-        
         int tank = 0;
         int start = 0;
-
-        for (int i = 0; i < gas.length; i++) {
+        for(int i = 0; i < gas.length; i ++) {
             tank += gas[i] - cost[i];
-
-            if (tank < 0) {
-                // Can't start from start ... i
+            if(tank < 0) {
                 tank = 0;
+                System.out.printf("i : %d, tank : %d \n", i, tank);
                 start = i + 1;
             }
         }
-
         return start;
     }
 }
