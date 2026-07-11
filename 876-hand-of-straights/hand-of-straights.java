ae123fa381c57a73da31ddf8f47b935274333c5a
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-07-11 19:01:51 +0530

    Time: 61 ms (45.75%) | Memory: 47.9 MB (73.77%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,36 +1,23 @@
 class Solution {
-
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        int handSize = hand.length;
-        if (handSize % groupSize != 0) {
-            return false;
+        if(hand.length % groupSize != 0) return false;
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        for(int num : hand){
+            treeMap.put(num , treeMap.getOrDefault(num , 0) + 1);
         }
 
-        // TreeMap to store the count of each card value
-        Map<Integer, Integer> cardCount = new TreeMap<>();
-        for (int i = 0; i < handSize; i++) {
-            cardCount.put(hand[i], cardCount.getOrDefault(hand[i], 0) + 1);
-        }
-
-        // Process the cards until the map is empty
-        while (cardCount.size() > 0) {
-            // Get the smallest card value
-            int current_card = cardCount.entrySet().iterator().next().getKey();
-            // Check each consecutive sequence of groupSize cards
-            for (int i = 0; i < groupSize; i++) {
-                // If a card is missing or has exhausted its occurrences
-                if (!cardCount.containsKey(current_card + i)) return false;
-                cardCount.put(
-                    current_card + i,
-                    cardCount.get(current_card + i) - 1
-                );
-                // Remove the card value if its occurrences are exhausted
-                if (cardCount.get(current_card + i) == 0) cardCount.remove(
-                    current_card + i
-                );
+        int count = hand.length / groupSize;
+        while(count > 0) {
+            int lowestElm = treeMap.firstKey();
+            for(int i = 0; i < groupSize; i ++){
+                int curRequiredNum = lowestElm + i;
+                if(!treeMap.containsKey(curRequiredNum)) return false;
+                int newFreq = (treeMap.get(curRequiredNum) - 1);
+                if(newFreq == 0) treeMap.remove(curRequiredNum);
+                else treeMap.put(curRequiredNum , newFreq);
             }
+            count --;
         }
-
-        return true;
+        return treeMap.size() == 0;
     }
 }
