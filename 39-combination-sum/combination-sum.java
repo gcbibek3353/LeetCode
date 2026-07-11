ec42cbf2202f82a03f8a4d981afffdd9911ee0c2
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-07-11 23:01:13 +0530

    Time: 2 ms (96.48%) | Memory: 45.8 MB (33.90%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,21 +2,19 @@ class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     List<Integer> curList = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        generate(0 , 0 , candidates , target);
+        generate(candidates ,  target ,  0 , 0);
         return ans;
     }
-
-    private void generate(int curSum , int curIndex, int[] candidates , int target) {
-        if(curSum > target || curIndex == candidates.length) return;
+    private void generate(int[] candidates , int target , int curSum ,int index){
         if(curSum == target){
-            List<Integer> curAns = new ArrayList<>();
-            for(int i = 0; i < curList.size(); i ++) curAns.add(curList.get(i));
-            ans.add(curAns);
+            ans.add(new ArrayList(curList));
             return;
         }
-        curList.add(candidates[curIndex]);
-        generate(curSum + candidates[curIndex], curIndex , candidates , target);
-        curList.remove(curList.size() - 1);
-        generate(curSum , curIndex + 1 , candidates , target);
+        if(curSum > target || index == candidates.length) return;
+        for(int i = index; i < candidates.length; i ++) {
+            curList.add(candidates[i]);
+            generate(candidates , target , curSum + candidates[i] , i);
+            curList.remove(curList.size() - 1);
+        }
     }
 }
