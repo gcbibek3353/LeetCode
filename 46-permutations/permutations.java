5144a7a03994e18b51dd62d116f93671788a6e1c
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-07-11 23:22:36 +0530

    Time: 1 ms (87.30%) | Memory: 45.4 MB (79.35%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,26 +1,24 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     List<Integer> curList = new ArrayList<>();
-    boolean[] visited;
     public List<List<Integer>> permute(int[] nums) {
-        visited = new boolean[nums.length];
-        generate(nums);
+        boolean[] visited = new boolean[nums.length];
+        generate(nums , visited);
         return ans;
     }
-    private void generate(int[] nums) {
+    private void generate(int[] nums , boolean[] visited) {
         if(curList.size() == nums.length){
             ans.add(new ArrayList(curList));
             return;
         }
-
         for(int i = 0; i < nums.length; i ++) {
-            if(!visited[i]){
-                curList.add(nums[i]);
+            if(!visited[i]) {
                 visited[i] = true;
-                generate(nums);
+                curList.add(nums[i]);
+                generate(nums , visited);
                 curList.remove(curList.size() - 1);
                 visited[i] = false;
-            } 
+            }
         }
     }
 }
