2852a074bf7484624dc1a55d4fd1d6e0bdf5b6a6
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-07-19 09:20:37 +0530

    Time: 3 ms (99.47%) | Memory: 46.6 MB (81.29%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,86 +1,41 @@
 class Solution {
-
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-
         List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses; i ++) adj.add(new ArrayList<>());
 
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        for (int[] pre : prerequisites) {
-            // [a, b] means b -> a
+        for(int[] pre : prerequisites) {
             adj.get(pre[1]).add(pre[0]);
         }
-
-        boolean[] visited = new boolean[numCourses];
         boolean[] path = new boolean[numCourses];
-
+        boolean[] visited = new boolean[numCourses];
         List<Integer> order = new ArrayList<>();
 
-        for (int i = 0; i < numCourses; i++) {
-
-            if (!visited[i]) {
-
-                if (detectCycle(i, visited, path, adj, order)) {
-                    return new int[0];
-                }
-
+        for(int i = 0; i < numCourses; i ++ ) {
+            if(!visited[i]){
+                // System.out.printf("i : %d , " ,i);
+                if(detectCycle(i , visited , path , adj, order)) return new int[0];
             }
         }
-
-        // DFS gives reverse topological order
         Collections.reverse(order);
-
-        int[] result = new int[numCourses];
-
-        for (int i = 0; i < numCourses; i++) {
-            result[i] = order.get(i);
+        int[] ans = new int[order.size()];
+        for(int i = 0; i < order.size(); i ++) {
+            ans[i] = order.get(i);
         }
+        return ans;
 
-        return result;
     }
 
+    private boolean detectCycle(int node, boolean[] visited , boolean[] path , List<List<Integer>> adj, List<Integer> order) {
+        visited[node] = path[node] = true;
 
-    private boolean detectCycle(
-        int node,
-        boolean[] visited,
-        boolean[] path,
-        List<List<Integer>> adj,
-        List<Integer> order
-    ) {
-
-        visited[node] = true;
-        path[node] = true;
-
-        for (int nextNode : adj.get(node)) {
-
-            // Cycle detected
-            if (path[nextNode]) {
-                return true;
-            }
-
-            // Explore unvisited node
-            if (!visited[nextNode]) {
-
-                if (detectCycle(
-                    nextNode,
-                    visited,
-                    path,
-                    adj,
-                    order
-                )) {
-                    return true;
-                }
-            }
+        List<Integer> neighbors = adj.get(node);
+        for(int nextNode : neighbors) {
+            // if(visited[nextNode]) return true;
+            if(!visited[nextNode] && detectCycle(nextNode , visited , path , adj,order)) return true;
+            if(path[nextNode]) return true;
         }
-
-        // Remove from current DFS recursion path
         path[node] = false;
-
-        // Add after processing all children
         order.add(node);
-
         return false;
     }
 }
