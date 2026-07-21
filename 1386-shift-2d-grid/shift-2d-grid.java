b775f8af8c92d11afe2479641c268591bb1b258c
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-07-21 11:56:04 +0530

    Time: 13 ms (5.73%) | Memory: 47.5 MB (8.72%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -1,58 +1,36 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         List<List<Integer>> ans = new ArrayList<>();
-        int r = grid.length; 
+        int r = grid.length;
         int c = grid[0].length;
-        for(int i = 0; i < r; i ++) ans.add(new ArrayList<>());
-        for(int i = 0; i < r; i ++){
-            for(int j = 0; j < c; j ++) {
-                ans.get(i).add(j,grid[i][j]);
-            }
-        }
+        int[][] dup = new int[r][c];
+        if(k == 0) dup = grid;
 
-        for(int count = 0; count < k; count ++){
-
-            int[] lastCol = new int[r];
-            int lastElm = ans.get(r-1).get(c-1);
-            // keeping copy of last column
-            for(int i = 0; i < r; i ++){
-                lastCol[i] = ans.get(i).get(c - 1);
+        for (int count = 0; count < k; count++) {
+            dup[0][0] = grid[r - 1][c - 1];
+            for (int i = 1; i < r; i++) {
+                dup[i][0] = grid[i - 1][c - 1];
             }
-            // copying over all elements except the first column
-            for(int i = c - 1; i > 0; i --) {
-                for(int j = 0; j < r; j ++) {
-                    int curValue = ans.get(j).get(i - 1);
-                    ans.get(j).set(i,curValue);
+            for (int i = 0; i < r; i++) {
+                for (int j = 1; j < c; j++) {
+                    dup[i][j] = grid[i][j - 1];
                 }
             }
-            // printArrayList(ans.get(0));
-            // printArrayList(ans.get(1));
-            // printArrayList(ans.get(2));
-            // filling first column
-            for(int i = r - 1; i > 0; i --) {
-                int curValue = lastCol[i - 1];
-                ans.get(i).set(0,curValue);
+            grid = new int[r][c];
+            for(int i = 0; i < r; i ++){
+                for(int j = 0; j < c; j ++) {
+                    grid[i][j] = dup[i][j];
+                }
             }
-            //
-            ans.get(0).set(0,lastElm);
         }
 
+        for (int i = 0; i < r; i++) {
+            List<Integer> curList = new ArrayList<>();
+            for (int j = 0; j < c; j++) {
+                curList.add(dup[i][j]);
+            }
+            ans.add(curList);
+        }
         return ans;
-    }
-
-    private void printArray(int[] arr) {
-        System.out.print("[");
-        for(int i = 0; i < arr.length; i ++){
-            System.out.print(arr[i] + ",");
-        }
-        System.out.println("]");
-    }
-
-    private void printArrayList(List<Integer> arr) {
-        System.out.print("[");
-        for(int i = 0; i < arr.size(); i ++){
-            System.out.print(arr.get(i) + ",");
-        }
-        System.out.println("]");
     }
 }
