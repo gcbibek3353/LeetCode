38fea13fe1851edc7a9f0919fb07a1ea7364c14f
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-07-30 09:01:41 +0530

    Time: 58 ms (11.62%) | Memory: 95.5 MB (69.11%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -1,18 +1,16 @@
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        Arrays.sort(points,(a,b) -> Integer.compare(a[0],b[0]));
-        int i = 0;
-        int count = 0;
-        while(i < points.length){
+        Arrays.sort(points,(a,b) -> Integer.compare(a[0], b[0]));
+        int mergeCount = 0;
+        for(int i = 0; i < points.length - 1; i ++) {
             int[] curInterval = points[i];
-            while(i < points.length - 1 && curInterval[1] >= points[i + 1][0]){
-            curInterval[0] = Math.max(curInterval[0],points[i + 1][0]);
-            curInterval[1] = Math.min(curInterval[1],points[i + 1][1]);
-            i ++;
-            } 
-            if(i >= points.length - 1 || curInterval[1] < points[i + 1][0])i ++;
-            count ++;
+            int[] nextInterval = points[i + 1];
+            if(curInterval[1] >= nextInterval[0]){
+                mergeCount ++;
+                nextInterval[0] = Math.max(curInterval[0], nextInterval[0]);
+                nextInterval[1] = Math.min(curInterval[1], nextInterval[1]);
+            }
         }
-        return count;
+        return points.length - mergeCount;
     }
 }
