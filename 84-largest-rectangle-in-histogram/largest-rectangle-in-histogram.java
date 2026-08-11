3ae0e4861fee4a6edaf9783e73ade8feeff599ca
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-08-11 11:23:10 +0530

    Time: 42 ms (85.71%) | Memory: 83 MB (11.55%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,40 +1,51 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] nse = getNSE(heights);
-        int[] pse = getPSE(heights);
-        int area = 0;
-        for (int i = 0; i < heights.length; i++) {
-            int curArea = (nse[i] - pse[i] - 1) * heights[i];
-            area = Math.max(area, curArea);
-        }
-        return area;
-    }
+        int[] NSE = NSE(heights);
+        int[] PSE = PSE(heights);
 
-    private int[] getNSE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-        for (int i = n - 1; i >= 0; i--) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
+        // for(int i = 0; i < heights.length; i ++)System.out.print(NSE[i] + " ");
+        // System.out.println();
+        // for(int i = 0; i < heights.length; i ++)System.out.print(PSE[i] + " ");
+        
+        int maxArea = 0;
+        for(int i = 0; i < heights.length; i ++) {
+            int width = NSE[i] - PSE[i] - 1;
+            int curArea = width * heights[i];
+            // System.out.print(maxArea);
+            maxArea = Math.max(maxArea , curArea);
+        }
+        return maxArea;
+    }
+    private int[] NSE(int[] heights) {
+        int[] ans = new int[heights.length];
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+    
+        for(int i = 0; i < heights.length; i ++){
+            while(!stack.isEmpty() && heights[stack.peek()] > heights[i]){
+                int prevIndex = stack.pop();
+                ans[prevIndex] = i;
             }
-            ans[i] = st.isEmpty() ? n : st.peek();
-            st.push(i);
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            ans[stack.pop()] = heights.length;
         }
         return ans;
     }
 
-    private int[] getPSE(int[] arr) {
-        int n = arr.length;
-        int[] ans = new int[n];
-        Stack<Integer> st = new Stack<>();
-
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
-                st.pop();
+    private int[] PSE(int[] heights) {
+        int[] ans = new int[heights.length];
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+    
+        for(int i = heights.length - 1; i >= 0; i --){
+            while(!stack.isEmpty() && heights[stack.peek()] > heights[i]){
+                int prevIndex = stack.pop();
+                ans[prevIndex] = i;
             }
-            ans[i] = st.isEmpty() ? -1 : st.peek();
-            st.push(i);
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            ans[stack.pop()] = -1;
         }
         return ans;
     }
