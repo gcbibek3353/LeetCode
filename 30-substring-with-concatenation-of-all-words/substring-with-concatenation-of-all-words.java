c96f2e2bb34e358066ddefb9f7f4e5ca9e1267f0
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-08-11 08:14:30 +0530

    Time: 11 ms (96.54%) | Memory: 47.4 MB (25.48%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,50 +1,79 @@
 class Solution {
+
     public List<Integer> findSubstring(String s, String[] words) {
+
+        Map<String, Integer> original = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
 
-        if (words.length == 0 || s.length() == 0) {
-            return ans;
-        }
-
         int wordSize = words[0].length();
-        int wordCount = words.length;
-        int N = s.length();
+        int totalWords = words.length;
+        int totalLength = wordSize * totalWords;
 
-        HashMap<String,Integer> originalCount = new HashMap<>();
-        for(int i = 0; i<words.length; i++){
-            originalCount.put(words[i], originalCount.getOrDefault(words[i],0)+1);
+        for (String str : words) {
+            original.put(str, original.getOrDefault(str, 0) + 1);
         }
 
-        for(int offset = 0; offset<wordSize; offset++){
-            HashMap<String,Integer> currentCount = new HashMap<>();
+        // We only need to start from wordSize different offsets
+        for (int offset = 0; offset < wordSize; offset++) {
+
             int start = offset;
-            int count = 0;
-            for(int end = offset; end + wordSize <= N; end += wordSize){
-                String currWord = s.substring(end, end + wordSize);
-                if(originalCount.containsKey(currWord)){
-                    currentCount.put(currWord, currentCount.getOrDefault(currWord,0)+1);
-                    count++;
+            int wordCount = 0;
 
-                    while(currentCount.get(currWord)>originalCount.get(currWord)){
-                        String startWord = s.substring(start,start+wordSize);
-                        currentCount.put(startWord, currentCount.get(startWord)-1);
-                        start+=wordSize;
-                        count--;                        
-                    }
+            Map<String, Integer> curMap = new HashMap<>();
 
-                    if(count == wordCount){
-                        ans.add(start);
-                    }
-                    
-                }
-                else{
-                    count = 0;
+            for (int end = offset; end + wordSize <= s.length(); end += wordSize) {
+
+                String curWord = s.substring(end, end + wordSize);
+
+                // Word is not present in words -> reset window
+                if (!original.containsKey(curWord)) {
+                    curMap.clear();
+                    wordCount = 0;
                     start = end + wordSize;
-                    currentCount.clear();
+                    continue;
+                }
+
+                curMap.put(
+                    curWord,
+                    curMap.getOrDefault(curWord, 0) + 1
+                );
+
+                wordCount++;
+
+                // Too many occurrences of curWord
+                while (curMap.get(curWord) > original.get(curWord)) {
+
+                    String firstWindowWord =
+                        s.substring(start, start + wordSize);
+
+                    curMap.put(
+                        firstWindowWord,
+                        curMap.get(firstWindowWord) - 1
+                    );
+
+                    start += wordSize;
+                    wordCount--;
+                }
+
+                // We have exactly all words
+                if (wordCount == totalWords) {
+                    ans.add(start);
+
+                    // Move window forward by one word
+                    String firstWindowWord =
+                        s.substring(start, start + wordSize);
+
+                    curMap.put(
+                        firstWindowWord,
+                        curMap.get(firstWindowWord) - 1
+                    );
+
+                    start += wordSize;
+                    wordCount--;
                 }
             }
-
         }
+
         return ans;
     }
 }
