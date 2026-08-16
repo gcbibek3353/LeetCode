92893d170637a94a62b27ec393cf6d0ee923856c
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-08-16 09:32:25 +0530

    Time: 4 ms (9.80%) | Memory: 47.1 MB (7.75%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,57 +1,57 @@
 class Solution {
-
-    int maxIslandSize = 0;
-    class Coords{
-        int row;
-        int col;
-        public Coords(int r , int c ) {
-            row = r;
-            col = c;
+    public class Coord{
+        int r;
+        int c;
+        public Coord(int r , int c) {
+            this.r = r;
+            this.c = c;
         }
     }
+    int[][] directions = {{-1 , 0}, {1 , 0}, {0, -1}, { 0 , 1}};
 
     public int maxAreaOfIsland(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
-        boolean[][] visited = new boolean[r][c];
-
-
-        for(int i = 0; i < r; i ++) {
-            for(int j = 0; j < c; j ++) {
-                if(grid[i][j] == 0 || visited[i][j]) continue;
-                bfs(i , j , visited, grid);
-            }
-        }
-        return maxIslandSize;
-    }
-
-    private void bfs(int i , int j , boolean[][] visited, int[][] grid) {
-        if(visited[i][j]) return;
-        Queue<Coords> q = new LinkedList<>();
-        q.offer(new Coords(i , j));
-        visited[i][j] = true;
-        int curIslandSize = 1;
-
-        int[][] directions = {{-1 , 0} , {1 , 0} , {0 , 1} , { 0 , -1}};
-        
-        while(!q.isEmpty()){
-            int queueSize = q.size();
-            for(int k = 0; k < queueSize; k ++) {
-                Coords curCoord = q.poll();
-                for(int[] dir : directions) {
-                    int curRow = curCoord.row + dir[0];
-                    int curCol = curCoord.col + dir[1];
-                    if(curRow < 0 || curRow >= grid.length) continue;
-                    if(curCol < 0 || curCol >= grid[0].length) continue;
-                    if(grid[curRow][curCol] == 0 || visited[curRow][curCol]) continue;
-                    // 0 or visited check
-                    curIslandSize ++;
-                    q.offer(new Coords(curRow , curCol));
-                    visited[curRow][curCol] = true;
+        int maxArea = 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i ++) {
+            for(int j = 0; j < grid[0].length; j ++) {
+                if(!visited[i][j] && grid[i][j] == 1){
+                    int curArea = bfs(i , j , grid, visited);
+                    maxArea = Math.max(curArea , maxArea);
                 }
             }
         }
-        // System.out.printf("i : %d , j : %d , curSize : %d \n", i , j , curIslandSize);
-        maxIslandSize = Math.max(maxIslandSize, curIslandSize);
+        return maxArea;
+    }
+
+    private int bfs(int i , int j , int[][] grid , boolean[][] visited) {
+        Queue<Coord> q = new LinkedList<>();
+        Coord firstCoord = new Coord(i , j);
+        q.offer(firstCoord);
+        int area = 1;
+        visited[i][j] = true;
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            for(int count = 0; count < size; count ++) {
+                Coord curCoord = q.poll();
+                for(int[] dir : directions) {
+                    int curRow = curCoord.r + dir[0];
+                    int curCol = curCoord.c + dir[1];
+                    if(
+                        curRow >= 0 && curRow < grid.length &&
+                        curCol >= 0 && curCol < grid[0].length &&
+                        grid[curRow][curCol] == 1 && 
+                        !visited[curRow][curCol]
+                    ){
+                        Coord newCoord = new Coord(curRow , curCol); 
+                        q.offer(newCoord);
+                        visited[curRow][curCol] = true;
+                        area ++;
+                        // System.out.printf("curRow : %d , curCol : %d , area : %d , i : %d , j : %d \n", curRow , curCol , area , i , j);
+                    } 
+                }
+            }
+        }
+        return area;
     }
 }
