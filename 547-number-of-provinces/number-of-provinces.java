d1c4fde25686eb058cb00f7a02a5961ee96482a1
Bivek Gharti <99399108+gcbibek3353@users.noreply.github.com>
2026-08-23 21:25:06 +0530

    Time: 4 ms (15.68%) | Memory: 47.4 MB (31.39%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,25 +1,27 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean[] isVisited = new boolean[n];
-        int ans = 0;
+        Set<Integer> visited = new HashSet<>();
+        int noOfProvince = 0;
 
-        for(int i = 0; i < n; i ++){
-            if(!isVisited[i]){
-                ans ++;
-                dfs(i , isConnected , isVisited);
+        for(int i = 0; i < isConnected.length; i ++) {
+            if(visited.contains(i)) continue;
+            noOfProvince ++;
+            Queue<Integer> q = new LinkedList<>();
+            q.offer(i);
+            while(!q.isEmpty()) {
+                int size = q.size();
+                for(int j = 0; j < size; j ++) {
+                    int topCity = q.poll();
+                    for(int k = 0; k < n; k ++) {
+                        if(isConnected[topCity][k] == 1 && topCity != k && !visited.contains(k)){
+                            visited.add(k);
+                            q.offer(k);
+                        } 
+                    }
+                }
             }
         }
-        return ans;
+        return noOfProvince;
     }
-
-    private void dfs(int i , int[][] isConnected , boolean[] isVisited){
-        if(isVisited[i]) return;
-        isVisited[i] = true;
-        int n = isConnected.length;
-        for(int j = 0; j < n; j ++) {
-            if(!isVisited[j] && isConnected[i][j] == 1) dfs(j,isConnected , isVisited);
-        }
-    }
-
 }
